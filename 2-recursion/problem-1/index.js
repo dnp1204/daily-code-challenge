1221a4d090fcd5ea51716712c80d6ee66c41bc0c
Doanh Pham <doanhpham1995@gmail.com>
2018-10-29 21:48:34 -0400

    count ways

--- 2-recursion/problem-1/index.js
+++ 2-recursion/problem-1/index.js
@@ -8,3 +8,52 @@
  * decoded as 'aaa', 'ka', and 'ak'. You can assume that the messages 
  * are decodable. For example '001' is not allowed
  */
+function countWaysOfDecoded(message) {
+  // return helper(message, message.length);
+  return helperDP(message, message.length);
+}
+
+function helper(message, k) {
+  if (k === 0) {
+    return 1;
+  }
+
+  s = message.length - k;
+  if (message[s] === "0") {
+    return 0;
+  }
+
+  let result = helper(message, k - 1);
+  if (k >= 2 && parseInt(message.substring(s, s + 2)) <= 26) {
+    result += helper(message, k - 2);
+  }
+
+  return result;
+}
+
+function helperDP(message, k, memo = []) {
+  if (k === 0) {
+    return 1;
+  }
+
+  s = message.length - k;
+  if (message[s] === "0") {
+    return 0;
+  }
+
+  if (memo[k]) {
+    return memo[k];
+  }
+
+  let result = helper(message, k - 1, memo);
+  if (k >= 2 && parseInt(message.substring(s, s + 2)) <= 26) {
+    result += helper(message, k - 2, memo);
+  }
+
+  memo[k] = result;
+  return result;
+}
+
+console.log(countWaysOfDecoded('111'));
+console.log(countWaysOfDecoded('11111111111111111111111'));
+console.log(countWaysOfDecoded('12345'));
