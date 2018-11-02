a639e0d0acf804b9c9258061ab0a7b5df4a0a8a1
Doanh Pham <doanhpham1995@gmail.com>
2018-11-02 13:02:52 -0400

    rename file

---
+++ 1-arrays/autocomplete.js
@@ -0,0 +1,13 @@
+/**
+ * Company: Twitter
+ * 
+ * Implement an autocomplete system. That is, given a query string s and 
+ * a set of all possible query strings, return all strings in the set that
+ * have s as prefix
+ * 
+ * For example, given the query string de an the set of strings [dog, deer, deal]
+ * returns [deer, deal]
+ * 
+ * Hint: Try preprocessing the dictionary into a more efficient data structures to 
+ * speed up queries
+ */
