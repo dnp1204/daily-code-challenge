b049b046a97c0d0a636cc3427f6b63d9ab80117b
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-01-24 11:07:55 -0500

    add more questions

---
+++ 13-other/sync_algorithm.js
@@ -0,0 +1,7 @@
+/**
+ * Comapny: Google
+ * 
+ * Implement a file syncing algorithm for two computers over a low-bandwidth network. 
+ * What if we know the files in the two computers are mostly the same?
+ */
+// TODO:
