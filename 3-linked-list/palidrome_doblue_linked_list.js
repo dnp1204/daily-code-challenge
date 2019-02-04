c5b5a2dbe16fc14bba1f140fcdb8aadef58986b2
Doanh <Pham>
2019-02-04 14:13:40 -0500

    add more questions

---
+++ 3-linked-list/palidrome_doblue_linked_list.js
@@ -0,0 +1,7 @@
+/**
+ * Company: Google.
+ *
+ * Determine whether a doubly linked list is a palindrome. What if it’s singly linked?
+ * For example, 1 -> 4 -> 3 -> 4 -> 1 returns true while 1 -> 4 returns false.
+ */
+// TODO:
