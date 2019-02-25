021d63d7fabb9d3011987a8b93aec56f80fcd065
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-25 13:41:28 -0500

    add one more solution for problem 96

--- 81-100/problem-96.js
+++ 81-100/problem-96.js
@@ -7,6 +7,27 @@
  */
 var rotate = function(nums, k) {
   if (nums.length === k) return nums;
+  rotate2(nums, k);
+};
+
+const rotateHelper = function(nums, start, end) {
+  while (start < end) {
+    const temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
+  }
+};
+
+const rotate2 = function(nums, k) {
+  k %= nums.length;
+  rotateHelper(nums, 0, nums.length - 1);
+  rotateHelper(nums, 0, k - 1);
+  rotateHelper(nums, k, nums.length - 1);
+};
+
+const rotate1 = function(nums, k) {
   for (let i = 0; i < k; i++) {
     let j = nums.length - 1;
     const last = nums[j];
