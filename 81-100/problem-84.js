66bc0041079f954b6468a541576a2552f7f374e5
Doanh Pham <doanhpham1995@gmail.com>
2019-02-25 22:42:23 -0500

    add solution for problem 84

--- 81-100/problem-84.js
+++ 81-100/problem-84.js
@@ -4,4 +4,29 @@
  * Determine whether a doubly linked list is a palindrome. What if it’s singly linked?
  * For example, 1 -> 4 -> 3 -> 4 -> 1 returns true while 1 -> 4 returns false.
  */
-// TODO:
+var isPalindrome = function(head) {
+  let fast, slow, next;
+  fast = slow = head;
+  prev = null;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+
+    next = slow.next;
+    slow.next = prev;
+    prev = slow;
+    slow = next;
+  }
+
+  if (fast) {
+    slow = slow.next;
+  }
+
+  while (prev && slow) {
+    if (prev.val !== slow.val) return false;
+    prev = prev.next;
+    slow = slow.next;
+  }
+
+  return true;
+};
