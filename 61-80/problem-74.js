06573aab89c56686fa28ce622a3f8dfddf95a95c
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-26 19:33:54 -0500

    solve more problems

--- 61-80/problem-74.js
+++ 61-80/problem-74.js
@@ -1,7 +1,8 @@
 /**
  * Comapny: Google
- * 
- * Implement a file syncing algorithm for two computers over a low-bandwidth network. 
+ *
+ * Implement a file syncing algorithm for two computers over a low-bandwidth network.
  * What if we know the files in the two computers are mostly the same?
+ *
+ * Solution with go: https://github.com/vaskoz/dailycodingproblem-go/blob/master/day59/problem.go
  */
-// TODO:
--- 61-80/problem-75.js
+++ 61-80/problem-75.js
@@ -1,10 +1,18 @@
 /**
  * Company: Square
- * 
- * Assume you have access to a function toss_biased() which returns 0 or 1 with a 
- * probability that's not 50-50 (but also not 0-100 or 100-0). You do not know the 
+ *
+ * Assume you have access to a function toss_biased() which returns 0 or 1 with a
+ * probability that's not 50-50 (but also not 0-100 or 100-0). You do not know the
  * bias of the coin.
- * 
+ *
  * Write a function to simulate an unbiased coin toss.
  */
-// TODO:
+var toss_unbiased = function() {
+  const val1 = toss_biased();
+  const val2 = toss_biased();
+
+  if (val1 === 0 && val2 === 1) return 0;
+  if (val1 === 1 && val2 === 0) return 1;
+
+  return toss_unbiased();
+};
