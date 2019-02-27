cfc455da7a91f57a256b9bc951b5309f041e567c
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-27 14:01:59 -0500

    solve problem 81

--- 81-100/problem-81.js
+++ 81-100/problem-81.js
@@ -15,4 +15,43 @@
  * exists(board, "ABCCED") returns true, exists(board, "SEE") returns true,
  * exists(board, "ABCB") returns false.
  */
-// TODO:
+var exist = function(board, word) {
+  if (!word) return true;
+  const visited = {};
+
+  const search = function(i, j, index) {
+    if (index === word.length) return true;
+    if (
+      i >= board.length ||
+      i < 0 ||
+      j >= board[0].length ||
+      j < 0 ||
+      board[i][j] !== word.charAt(index) ||
+      visited[`${i}${j}`]
+    ) {
+      return false;
+    }
+
+    visited[`${i}${j}`] = true;
+    if (
+      search(i - 1, j, index + 1) ||
+      search(i + 1, j, index + 1) ||
+      search(i, j - 1, index + 1) ||
+      search(i, j + 1, index + 1)
+    ) {
+      return true;
+    }
+    visited[`${i}${j}`] = false;
+    return false;
+  };
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j] === word.charAt(0) && search(i, j, 0)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+};
