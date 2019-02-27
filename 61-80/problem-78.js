771ffe9050aeef90e01d4c6d44dbaa25013e1d57
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-26 19:11:29 -0500

    solve problem 78

--- 61-80/problem-78.js
+++ 61-80/problem-78.js
@@ -11,4 +11,24 @@
  *    4   5
  * It should return [[1, 2], [1, 3, 4], [1, 3, 5]].
  */
-// TODO:
+var binaryTreePaths = function(root) {
+  var helper = function(root, path = '', result = []) {
+    if (!root) return result;
+
+    if (!root.left && !root.right) {
+      result.push(path + root.val);
+    }
+
+    if (root.left) {
+      helper(root.left, path + root.val + '->', result);
+    }
+
+    if (root.right) {
+      helper(root.right, path + root.val + '->', result);
+    }
+
+    return result;
+  };
+
+  return helper(root);
+};
