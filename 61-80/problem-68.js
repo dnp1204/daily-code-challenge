fe45d8cfb7c394ca80dfd5aac1989af052b6090b
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-28 11:44:40 -0500

    fix some style

--- 61-80/problem-68.js
+++ 61-80/problem-68.js
@@ -1,5 +1,5 @@
 /**
- * Company: Two Sigma
+ * Company: Two Sigma.
  *
  * Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform
  * probability, implement a function rand5() that returns an integer from 1 to 5 (inclusive)
