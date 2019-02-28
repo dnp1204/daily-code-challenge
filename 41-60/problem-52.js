469c7679b3170b33d8af31c46e25afbd79732505
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-28 15:06:55 -0500

    solve problem 52

--- 41-60/problem-52.js
+++ 41-60/problem-52.js
@@ -1,13 +1,26 @@
 /**
- * Company: Google
- * 
- * Given the root of a binary tree, return a deepest node. For example, 
+ * Company: Google.
+ *
+ * Given the root of a binary tree, return a deepest node. For example,
  * in the following tree, return d.
- * 
+ *
  *     a
  *    / \
  *   b   c
  *  /
  * d
  */
-// TODO:
+var deepestNode = function(root) {
+  if (!root) return null;
+  const queue = [root];
+  let result = root;
+
+  while (queue.length) {
+    const node = queue.shift();
+    if (node.left) queue.push(node.left);
+    if (node.right) queue.push(node.right);
+    result = node;
+  }
+
+  return result;
+};
