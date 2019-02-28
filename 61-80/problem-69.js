e9002b03cbe3573e810819f50c3b5f009f216e76
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-28 10:52:35 -0500

    solve proble 69

--- 61-80/problem-69.js
+++ 61-80/problem-69.js
@@ -13,4 +13,36 @@
  * 1 1 0 0 1
  * 1 1 0 0 1
  */
-// TODO:
+var numIslands = function(grid) {
+  if (!grid.length) return 0;
+  let count = 0;
+  const numOfRows = grid.length;
+  const numOfCols = grid[0].length;
+
+  const dfsMarking = function(i, j) {
+    if (
+      i < 0 ||
+      j < 0 ||
+      i >= numOfRows ||
+      j >= numOfCols ||
+      grid[i][j] !== '1'
+    )
+      return;
+    grid[i][j] = '0';
+    dfsMarking(i + 1, j);
+    dfsMarking(i - 1, j);
+    dfsMarking(i, j + 1);
+    dfsMarking(i, j - 1);
+  };
+
+  for (let i = 0; i < numOfRows; i++) {
+    for (let j = 0; j < numOfCols; j++) {
+      if (grid[i][j] === '1') {
+        dfsMarking(i, j);
+        count++;
+      }
+    }
+  }
+
+  return count;
+};
