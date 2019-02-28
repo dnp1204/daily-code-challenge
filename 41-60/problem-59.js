6412232648cf6afae89ea57b9d8f4690782cc49b
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-28 15:46:23 -0500

    solve problem 59

--- 41-60/problem-59.js
+++ 41-60/problem-59.js
@@ -1,7 +1,22 @@
 /**
- * Company: Dropbox
+ * Company: Dropbox.
  *
- * Given the root to a binary search tree, find the second
- * largest node in the tree
+ * Given the root to a binary search tree, find the second largest node in the tree
  */
-// TODO:
+function findSecondLargest(root) {
+  const largest = -1;
+  const second = -1;
+
+  const helper = function(root) {
+    if (!root) return second;
+    const val = root.val;
+    if (val > largest) {
+      second = largest;
+      largest = val;
+    } else if (val > second) {
+      second = val;
+    }
+  };
+
+  return helper(root);
+}
