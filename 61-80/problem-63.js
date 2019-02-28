aa2cfda70d2932a0113c6096fcdf902fe24e21bc
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-28 14:12:41 -0500

    solve problem 63

--- 61-80/problem-63.js
+++ 61-80/problem-63.js
@@ -1,10 +1,10 @@
 /**
- * Company: Google
- * 
- * A unival tree (which stands for 'universal value') is a tree where 
+ * Company: Google.
+ *
+ * A unival tree (which stands for 'universal value') is a tree where
  * all nodes under it have the same value.
  * Given the root to binary tree, count the number of unival subtrees
- * 
+ *
  * For example, the following tree has 5 unival subtrees:
  *          0
  *         / \
@@ -14,4 +14,17 @@
  *         / \
  *        1   1
  */
-// TODO:
+function countUnivalSubtrees(root) {
+  let count = 0;
+
+  const helper = function(root) {
+    if (!root) return true;
+    if (!helper(root.left) || !helper(root.right)) return false;
+    if (root.left && root.value !== root.left.value) return false;
+    if (root.right && root.value !== root.right.value) return false;
+    count++;
+    return true;
+  };
+
+  return count;
+}
