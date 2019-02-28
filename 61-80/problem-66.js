bded09d80ac56067bfdb073b4a4392e3a5f2b88a
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-28 12:33:47 -0500

    solve problem 66

--- 61-80/problem-66.js
+++ 61-80/problem-66.js
@@ -1,8 +1,24 @@
 /**
- * Company: Microsoft
- * 
+ * Company: Microsoft.
+ *
  * A number is considered perfect if its digits sum up to exactly 10.
  * Given a positive integer n, return the n-th perfect number.
- * For example, given 1, you should return 19. Given 2, you should return 28.
+ *
+ * For example, given 1, you should return 19. Given 2, you should
+ * return 28.
  */
-// TODO:
+function findNth(n) {
+  let count = 0;
+  for (let curr = 19; ; curr += 9) {
+    // Find sum of digits in current no.
+    let sum = 0;
+    for (let x = curr; x > 0; x = x / 10) sum = sum + (x % 10);
+
+    // If sum is 10, we increment count
+    if (sum == 10) count++;
+
+    // If count becomes n, we return current
+    // number.
+    if (count == n) return curr;
+  }
+}
