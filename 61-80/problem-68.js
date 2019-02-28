bcb08919ea185516dd4bc4e04b0fd83651fc6d03
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-28 11:13:28 -0500

    solve problem 68

--- 61-80/problem-68.js
+++ 61-80/problem-68.js
@@ -1,7 +1,11 @@
 /**
  * Company: Two Sigma
- * 
- * Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform 
+ *
+ * Using a function rand7() that returns an integer from 1 to 7 (inclusive) with uniform
  * probability, implement a function rand5() that returns an integer from 1 to 5 (inclusive)
  */
-// TODO:
+const rand7 = function() {
+  let result = 6;
+  while (result >= 6) result = rand(7);
+  return result;
+};
