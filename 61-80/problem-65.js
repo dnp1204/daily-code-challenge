c767ba2a1186540462d36a8137a9f0d9dfa9fc49
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-28 12:39:57 -0500

    solve proble 65

--- 61-80/problem-65.js
+++ 61-80/problem-65.js
@@ -1,7 +1,9 @@
 /**
  * Company: Facebook.
- * 
- * Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0, 
+ *
+ * Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0,
  * using only mathematical or bit operations. You can assume b can only be 1 or 0.
  */
-// TODO:
+function getNum(x, y, b) {
+  return x * b + y * Math.abs(b - 1);
+}
