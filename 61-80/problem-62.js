a6ae3d74452f9b113b10af94d0907dec11ff325e
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-02-28 14:23:58 -0500

    solve problem 62

--- 61-80/problem-62.js
+++ 61-80/problem-62.js
@@ -1,11 +1,35 @@
 /**
- * Company: Google
- * 
- * Implement integer exponentiation. That is, implement the pow(x, y) function, 
+ * Company: Google.
+ *
+ * Implement integer exponentiation. That is, implement the pow(x, y) function,
  * where x and y are integers and returns x^y.
- * 
+ *
  * Do this faster than the naive method of repeated multiplication.
- * 
+ *
  * For example, pow(2, 10) should return 1024.
  */
-// TODO:
+function pow(x, y) {
+  if (!x) return 0;
+  else if (!y) return 1;
+
+  let yAbs = Math.abs(y);
+  let currentPow = 1;
+  let prevResult = 0;
+  let prevPow = 0;
+  let result = x;
+
+  while (currentPow <= yAbs) {
+    prevResult = result;
+    prevPow = currentPow;
+    result *= result;
+    currentPow *= 2;
+  }
+
+  prevResult *= pow(x, yAbs - prevPow);
+
+  if (y !== yAbs) {
+    return 1 / prevResult;
+  }
+
+  return prevResult;
+}
