ac974576f8764b89c0da78ef2f1787c9cb9119d7
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-04 14:35:35 -0500

    fix problem 53

--- 41-60/problem-53.js
+++ 41-60/problem-53.js
@@ -1,6 +1,6 @@
 /**
- * Company: Apple
- * 
+ * Company: Apple.
+ *
  * Given a tree, find the largest tree/subtree that is a BST.
  * Given a tree, return the size of the largest tree/subtree that is a BST.
  */
