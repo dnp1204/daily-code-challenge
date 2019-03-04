a7ea96a5084a53d2e64e4e45f91fb023e64d799e
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-04 14:33:56 -0500

    solve problem 54

--- 41-60/problem-54.js
+++ 41-60/problem-54.js
@@ -1,15 +1,15 @@
 /**
- * Company: Google
- * 
+ * Company: Google.
+ *
  * Invert a binary tree.
- * 
+ *
  * For example, given the following tree:
  *
- *    a
- *   / \
- *  b   c
- * / \  /
- * d   e f
+ *     a
+ *    / \
+ *   b   c
+ *  / \  /
+ * d  e f
  * should become:
  *
  *     a
@@ -18,4 +18,14 @@
  *  /  / \
  * f  e  d
  */
-// TODO:
+var invertTree = function(root) {
+  return helper(root);
+};
+
+var helper = function(root) {
+  if (!root) return root;
+  const temp = root.left;
+  root.left = helper(root.right);
+  root.right = helper(temp);
+  return root;
+};
