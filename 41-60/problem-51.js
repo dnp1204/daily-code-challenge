6b1a6ff31d09e70282c7d4303c867f111efee0dd
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-04 15:18:17 -0500

    add linked url

--- 41-60/problem-51.js
+++ 41-60/problem-51.js
@@ -1,9 +1,11 @@
 /**
- *  Company: LinkedIn
- * 
+ *  Company: LinkedIn.
+ *
  * Determine whether a tree is a valid binary search tree.
- * A binary search tree is a tree with two children, left and right, and satisfies the 
- * constraint that the key in the left child must be less than or equal to the root and 
+ * A binary search tree is a tree with two children, left and right, and satisfies the
+ * constraint that the key in the left child must be less than or equal to the root and
  * the key in the right child must be greater than or equal to the root.
+ *
+ * Leetcode: https://leetcode.com/problems/validate-binary-search-tree/
  */
 // TODO:
