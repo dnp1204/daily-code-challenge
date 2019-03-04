08e04ee74d6e8bff59500833d08e826f0f9dc428
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-04 13:54:09 -0500

    solve problem 57

--- 41-60/problem-57.js
+++ 41-60/problem-57.js
@@ -7,4 +7,19 @@
  *
  * Leetcode: https://leetcode.com/problems/binary-tree-maximum-path-sum/
  */
-// TODO:
+var maxPathSum = function(root) {
+  let max = -Infinity;
+  const helper = function(node) {
+    if (!node) return 0;
+    const left = helper(node.left);
+    const right = helper(node.right);
+    const maxLR = Math.max(left, right);
+    const maxNodeLR = Math.max(maxLR + node.val, node.val);
+    const maxAll = Math.max(maxNodeLR, left + right + node.val);
+    const maxPath = Math.max(maxAll, maxNodeLR);
+    max = Math.max(max, maxPath);
+    return maxNodeLR;
+  };
+  helper(root);
+  return max === -Infinity ? 0 : max;
+};
