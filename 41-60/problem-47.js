a8b562b83540e2233677dd724aaffec708d80e2f
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-05 14:09:40 -0500

    start problem 47

--- 41-60/problem-47.js
+++ 41-60/problem-47.js
@@ -1,18 +1,20 @@
 /**
- * Company: Facebook
- * 
+ * Company: Facebook.
+ *
  * Implement regular expression matching with the following special characters:
  * . (period) which matches any single character
  * * (asterisk) which matches zero or more of the preceding element
- * 
- * That is, implement a function that takes in a string and a valid regular 
+ *
+ * That is, implement a function that takes in a string and a valid regular
  * expression and returns whether or not the string matches the regular expression.
- * 
- * For example, given the regular expression "ra." and the string "ray", your function 
- * should return true. The same regular expression on the string "raymond" should 
+ *
+ * For example, given the regular expression "ra." and the string "ray", your function
+ * should return true. The same regular expression on the string "raymond" should
  * return false.
- * 
- * Given the regular expression ".*at" and the string "chat", your function should 
+ *
+ * Given the regular expression ".*at" and the string "chat", your function should
  * return true. The same regular expression on the string "chats" should return false.
+ *
+ * Leetcode: https://leetcode.com/problems/regular-expression-matching/
  */
 // TODO:
