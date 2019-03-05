85c4e6327d3dc49d9fcd186b1d395069e10f894f
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-05 14:08:04 -0500

    solve problem 48

--- 41-60/problem-48.js
+++ 41-60/problem-48.js
@@ -1,10 +1,24 @@
 /**
- * Company: Microsoft
- * 
- * Using a read7() method that returns 7 characters from a file, 
+ * Company: Microsoft.
+ *
+ * Using a read7() method that returns 7 characters from a file,
  * implement readN(n) which reads n characters.
- * 
- * For example, given a file with the content “Hello world”, 
+ *
+ * For example, given a file with the content “Hello world”,
  * three read7() returns “Hello w”, “orld” and then “”.
  */
-// TODO:
+function readN(content, n) {
+  let result = '';
+  let nextContent = read7();
+  if (result.length + nextContent.length <= n && nextContent) {
+    result += nextContent;
+    nextContent = read7();
+  }
+
+  if (nextContent) {
+    let nextLength = n - nextContent.length;
+    result += content.substring(result.length - 1, nextLength);
+  }
+
+  return result;
+}
