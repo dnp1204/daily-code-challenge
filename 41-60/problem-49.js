6301889e1be52e6c43c2c588ac070a69900b39a2
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-05 13:52:20 -0500

    solve problem 49

--- 41-60/problem-49.js
+++ 41-60/problem-49.js
@@ -1,11 +1,37 @@
 /**
- * Company: Microsoft
- * 
+ * Company: Microsoft.
+ *
  * Implement a URL shortener with the following methods:
  * shorten(url), which shortens the url into a six-character alphanumeric string, such as zLg6wl.
- * restore(short), which expands the shortened string into the original url. If no such shortened 
+ * restore(short), which expands the shortened string into the original url. If no such shortened
  * string exists, return null.
- * 
+ *
  * Hint: What if we enter the same URL twice?
  */
-// TODO:
+const crypto = require('crypto');
+
+class UrlShortener {
+  constructor() {
+    this.shortToUrlMap = {};
+    this.urls = {};
+    this.hash = crypto.createHash('sha256');
+    this.prefix = 'http://urlshort.li/';
+  }
+
+  shorten(url) {
+    if (this.urls[url]) {
+      return this.urls[url];
+    }
+    const hashUrl = this.hash
+      .update(url)
+      .digest('hex')
+      .substring(0, 6);
+    this.shortToUrlMap[hashUrl] = url;
+    this.urls[url] = this.prefix + hashUrl;
+    return this.prefix + hashUrl;
+  }
+
+  restore(url) {
+    return this.shortToUrlMap[url.replace(this.prefix, '')] || null;
+  }
+}
