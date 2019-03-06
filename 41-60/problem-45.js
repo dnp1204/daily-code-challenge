88f0d3190b67e7c790dae21d54999aeca99cb14f
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-06 11:34:47 -0500

    solve problem 45

--- 41-60/problem-45.js
+++ 41-60/problem-45.js
@@ -1,5 +1,5 @@
 /**
- * Company: Quora
+ * Company: Quora.
  *
  * Given a string, find the palindrome that can be made by
  * inserting the fewest number of characters as possible
@@ -14,5 +14,35 @@
  * first alphabetically.
  *
  * As another example, given the string "google", you should return "elgoogle".
+ *
+ * Youtube: https://www.youtube.com/watch?v=DOnK40BvazI
  */
-// TODO:
+function getMinAddition(s) {
+  const helper = function(s, l, h) {
+    if (l > h) return Number.MAX_SAFE_INTEGER;
+    if (l === h) return 0;
+    if (l === h - 1) return s.charAt[l] === s.charAt[h] ? 0 : 1;
+    if (s.charAt(s) === s.charAt[l]) {
+      return helper(s, l + 1, h - 1);
+    }
+    return Math.min(helper(s, l + 1, h), helper(s, l, h - 1)) + 1;
+  };
+
+  const dynamicHelper = function(s) {
+    const length = s.length;
+    const table = Array(length).fill(Array(length).fill(0));
+    for (let gap = 1; gap < length; gap++) {
+      for (let l = 0, h = gap; h < length; l++, h++) {
+        if (s.charAt(l) === s.charAt(h)) {
+          table[l][h] = table[l + 1][h - 1];
+        } else {
+          table[l][h] = Math.min(table[l + 1][h], table[l][h - 1]) + 1;
+        }
+      }
+    }
+
+    return table[0][length - 1];
+  };
+
+  return dynamicHelper(s, 0, s.length);
+}
--- 41-60/problem-46.js
+++ 41-60/problem-46.js
@@ -1,10 +1,12 @@
 /**
- * Company: Google
- * 
- * Given a string of parentheses, write a function to compute the minimum number of parentheses 
+ * Company: Google.
+ *
+ * Given a string of parentheses, write a function to compute the minimum number of parentheses
  * to be removed to make the string valid (i.e. each open parenthesis is eventually closed).
- * 
- * For example, given the string "()())()", you should return 1. Given the string ")(", you 
+ *
+ * For example, given the string "()())()", you should return 1. Given the string ")(", you
  * should return 2, since we must remove all of them.
+ *
+ * Leetcode: https://leetcode.com/problems/remove-invalid-parentheses/
  */
 // TODO:
