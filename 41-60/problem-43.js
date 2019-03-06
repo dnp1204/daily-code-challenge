e4fb1e66da7df6f30cedf7ed4c28af79d8464981
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-06 17:10:41 -0500

    solve problem 43

--- 41-60/problem-43.js
+++ 41-60/problem-43.js
@@ -1,5 +1,5 @@
 /**
- * Company: Amazon
+ * Company: Amazon.
  *
  * Run-length encoding is a fast and simple method of encoding
  * strings. The basic idea is to represent repeated successive
@@ -10,4 +10,35 @@
  * to be encoded have no digits and consists solely of alphabetic
  * characters. You can assume the string to be decoded is valid.
  */
-// TODO:
+const encode = function(s) {
+  let result = '';
+  let count = 0;
+  let index = 0;
+  let pointer = 0;
+
+  while (index < s.length) {
+    if (s.charAt(index) === s.charAt(pointer)) {
+      count++;
+    } else {
+      result = result + count + s.charAt(pointer);
+      pointer = index;
+      count = 1;
+    }
+    index++;
+  }
+
+  result = result + count + s.charAt(pointer);
+
+  return result;
+};
+
+const decode = function(s) {
+  let result = '';
+  for (let index = 0; index < s.length; index += 2) {
+    const letter = s.charAt(index + 1);
+    for (let j = 0; j < parseInt(s.charAt(index)); j++) {
+      result += letter;
+    }
+  }
+  return result;
+};
