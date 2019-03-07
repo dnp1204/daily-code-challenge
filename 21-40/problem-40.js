37efc1090ba8b1b5c16c7558141ff8f9c1e9f8cd
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-07 14:50:06 -0500

    solve problem 40

--- 21-40/problem-40.js
+++ 21-40/problem-40.js
@@ -10,4 +10,32 @@
  *
  * Leetcode: https://leetcode.com/problems/letter-combinations-of-a-phone-number/
  */
-// TODO:
+var letterCombinations = function(digits) {
+  const digitsToLetters = {
+    '2': ['a', 'b', 'c'],
+    '3': ['d', 'e', 'f'],
+    '4': ['g', 'h', 'i'],
+    '5': ['j', 'k', 'l'],
+    '6': ['m', 'n', 'o'],
+    '7': ['p', 'q', 'r', 's'],
+    '8': ['t', 'u', 'v'],
+    '9': ['w', 'x', 'y', 'z']
+  };
+  const nums = digits.split('');
+  const result = [];
+
+  const helper = function(nums) {
+    if (nums.length === 0) return [];
+    if (nums.length === 1) return digitsToLetters[nums[0]];
+    const letters = helper(nums.slice(1, nums.length));
+    const temp = [];
+    for (const letterLeft of digitsToLetters[nums[0]]) {
+      for (const letterRight of letters) {
+        temp.push(letterLeft + letterRight);
+      }
+    }
+    return temp;
+  };
+
+  return helper(nums);
+};
