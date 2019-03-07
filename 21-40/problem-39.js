bc58e4320dc7c13ba6b8a0b81a0297a5d84c6b6a
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-07 14:54:25 -0500

    start problem 39

--- 21-40/problem-39.js
+++ 21-40/problem-39.js
@@ -1,13 +1,13 @@
 /**
- * Company: Google
+ * Company: Google.
  *
- * The edit distance between two strings refers to the
- * minimum number of character insertions, deletions,
- * and substitutions required to change one string to
- * the other. For example, the edit distance between
- * “kitten” and “sitting” is three: substitute the “k”
- * for “s”, substitute the “e” for “i”, and append a “g”.
- *
+ * The edit distance between two strings refers to the minimum number of character
+ * insertions, deletions, and substitutions required to change one string to the
+ * other. For example, the edit distance between “kitten” and “sitting” is three:
+ * substitute the “k” for “s”, substitute the “e” for “i”, and append a “g”.
  * Given two strings, compute the edit distance between them.
+ *
+ * Leetcode: https://leetcode.com/problems/edit-distance/
+ * Yotube: https://www.youtube.com/watch?v=We3YDTzNXEk
  */
 // TODO:
