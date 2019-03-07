0bf6b48abc11d043fa74e0312d724cb001b51e47
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-07 11:46:00 -0500

    solve problem 42

--- 41-60/problem-42.js
+++ 41-60/problem-42.js
@@ -25,4 +25,41 @@
  *
  * Leetcode: https://leetcode.com/problems/text-justification/
  */
-// TODO:
+var fullJustify = function(words, maxWidth) {
+  let res = [],
+    current = [],
+    numOfLetters = 0;
+  words.forEach(word => {
+    if (numOfLetters + word.length + current.length > maxWidth) {
+      const remainingSpaces = maxWidth - numOfLetters;
+      if (current.length === 1) {
+        for (let i = 0; i < remainingSpaces; i++) {
+          current[0] += ' ';
+        }
+      } else {
+        for (let i = 0; i < remainingSpaces; i++) {
+          let index = i % (current.length - 1);
+          if (index < 0) {
+            index = 1;
+          }
+          current[index] += ' ';
+        }
+      }
+      res.push(current.join(''));
+      current = [];
+      numOfLetters = 0;
+    }
+    current.push(word);
+    numOfLetters += word.length;
+  });
+
+  if (current.length) {
+    let lastLine = current.join(' ');
+    for (let i = lastLine.length; i < maxWidth; i++) {
+      lastLine += ' ';
+    }
+    res.push(lastLine);
+  }
+
+  return res;
+};
