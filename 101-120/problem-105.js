d0c13d007d6c56fd77bfa2db156599b8436955d8
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-07 13:57:26 -0500

    solve problem 105

--- 101-120/problem-105.js
+++ 101-120/problem-105.js
@@ -12,5 +12,45 @@
  * Return 4.
  *
  * Leetcode: https://leetcode.com/problems/maximal-rectangle/
+ * Youtube: https://www.youtube.com/watch?v=g8bSdXCG-lA
  */
-// TODO:
+function maximalRectangle(matrix) {
+  if (
+    matrix == null ||
+    matrix.length == 0 ||
+    matrix[0] == null ||
+    matrix[0].length == 0
+  ) {
+    return 0;
+  }
+  const heights = Array(matrix[0].length).fill(0);
+  let ans = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === '0') heights[j] = 0;
+      else heights[j]++;
+    }
+    ans = Math.max(ans, calcArea(heights));
+  }
+  return ans;
+}
+
+function calcArea(heights) {
+  if (!heights || heights.length === 0) return 0;
+  let ans = 0;
+  let stack = [];
+  stack.push(-1);
+
+  for (let i = 0; i <= heights.length; i++) {
+    while (
+      stack[stack.length - 1] !== -1 &&
+      (i == heights.length || heights[i] < heights[stack[stack.length - 1]])
+    ) {
+      let index = stack.pop();
+      let start = stack[stack.length - 1] + 1;
+      ans = Math.max(ans, heights[index] * (i - start));
+    }
+    stack.push(i);
+  }
+  return ans;
+}
