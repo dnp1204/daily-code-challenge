a54482cc71838657806c5f7a61290c7cca331af7
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-07 12:16:44 -0500

    add problem 105

---
+++ 101-120/problem-105.js
@@ -0,0 +1,16 @@
+/**
+ * Company: Google.
+ *
+ * Given an N by M matrix consisting only of 1's and 0's, find the largest rectangle
+ * containing only 1's and return its area.
+ *
+ * For example, given the following matrix:
+ * [[1, 0, 0, 0],
+ * [1, 0, 1, 1],
+ * [1, 0, 1, 1],
+ * [0, 1, 0, 0]]
+ * Return 4.
+ *
+ * Leetcode: https://leetcode.com/problems/maximal-rectangle/
+ */
+// TODO:
