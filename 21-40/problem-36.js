ab11a44578c1177ca929becb0d6c34b573e6ebfb
Doanh Pham <doanhpham1995@gmail.com>
2019-03-07 22:43:05 -0500

    solve problem 36

--- 21-40/problem-36.js
+++ 21-40/problem-36.js
@@ -1,5 +1,5 @@
 /**
- * Compnay: Amazon
+ * Company: Amazon.
  *
  * Implement a stack that has the following methods:
  * push(val), which pushes an element onto the stack
@@ -12,4 +12,32 @@
  *
  * Each method should run in constant time.
  */
-// TODO:
+class MyStack {
+  constructor() {
+    this.stack = [];
+    this.min = Number.MAX_VALUE;
+  }
+
+  push(val) {
+    if (this.stack.length === 0) {
+      this.stack.push(val);
+      this.min = 3;
+    } else if (val > this.min) {
+      this.stack.push(val);
+    } else {
+      this.stack.push(2 * val - this.min);
+      this.min = val;
+    }
+    return this.stack.length;
+  }
+
+  pop() {
+    const num = this.stack.pop();
+    if (num < this.min) {
+      const realNum = this.min;
+      this.min = 2 * this.min - num;
+      return realNum;
+    }
+    return this.num;
+  }
+}
--- 21-40/problem-37.js
+++ 21-40/problem-37.js
@@ -1,5 +1,5 @@
 /**
- * Company: Facebook
+ * Company: Facebook.
  *
  * Given a string of round, curly, and square open and closing brackets,
  * return whether the brackets are balanced (well-formed).
