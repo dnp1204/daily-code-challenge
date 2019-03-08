08ad33d54301e0e038bf60faa2b789765f79d253
Doanh Pham <doanhpham1995@gmail.com>
2019-03-07 22:56:30 -0500

    solve problem 35

--- 21-40/problem-35.js
+++ 21-40/problem-35.js
@@ -1,8 +1,27 @@
 /**
- * Company: Apple
- * 
- * Implement a queue using two stacks. Recall that a queue is a FIFO (first-in, first-out) 
- * data structure with the following methods: enqueue, which inserts an element into the 
- * queue, and dequeue, which removes it.
+ * Company: Apple.
+ *
+ * Implement a queue using two stacks. Recall that a queue is a FIFO
+ * (first-in, first-out) data structure with the following methods:
+ * enqueue, which inserts an element into the queue, and dequeue,
+ * which removes it.
  */
-// TODO:
+class MyQueue {
+  constructor() {
+    this.stack1 = [];
+    this.stack2 = [];
+  }
+
+  enqueue(value) {
+    this.stack1.push(value);
+  }
+
+  dequeue() {
+    if (!this.stack1.length && !this.stack2.length) return null;
+    if (this.stack2.length) return this.stack2.pop();
+    while (this.stack1.length) {
+      this.stack2.push(this.stack1.pop());
+    }
+    return this.stack2.pop();
+  }
+}
