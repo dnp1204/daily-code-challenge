ab458cdbd77289958714fbd1e5540b00aa196fff
Doanh Pham <doanhpham1995@gmail.com>
2019-03-07 22:57:33 -0500

    solve problem 34

--- 21-40/problem-34.js
+++ 21-40/problem-34.js
@@ -1,4 +1,3 @@
-
 function pivot(arr, start = 0, end = arr.length - 1) {
   const swap = (arr, idx1, idx2) => {
     [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
@@ -20,32 +19,15 @@ function pivot(arr, start = 0, end = arr.length - 1) {
   return swapIdx;
 }
 
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right); //3
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
 
-function quickSort(arr, left = 0, right = arr.length -1){
-    if(left < right){
-        let pivotIndex = pivot(arr, left, right) //3
-        //left
-        quickSort(arr,left,pivotIndex-1);
-        //right
-        quickSort(arr,pivotIndex+1,right);
-      }
-     return arr;
-} 
-           
-quickSort([100,-3,2,4,6,9,1,2,5,3,23])
-
-
-
-
-// [4,6,9,1,2,5,3]
-// [3,2,1,4,6,9,5]
-//        4
-//  3,2,1    6,9,5
-//      3      6
-//  2,1      5  9
-//    2
-//  1
-
-
-
-
+quickSort([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23]);
