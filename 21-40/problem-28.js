eb5ef43ed19904758ab834d0bf767282cdcdf3a2
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-11 11:18:28 -0400

    solve problem 28

--- 21-40/problem-28.js
+++ 21-40/problem-28.js
@@ -1,6 +1,31 @@
 /**
- * Company: Google
- * 
+ * Company: Google.
+ *
  * Given the head of a singly linked list, reverse it in-place
  */
-// TODO:
+var reverseList = function(head) {
+  return helperRecursive(head, null);
+};
+
+function helperRecursive(head, previous) {
+  if (!head) return previous;
+  const next = head.next;
+  head.next = previous;
+  previous = head;
+  return helperRecursive(next, previous);
+}
+
+function helperIterative(head) {
+  let previous = null;
+  let current = head;
+  let next = null;
+
+  while (current) {
+    next = current.next;
+    current.next = previous;
+    previous = current;
+    current = next;
+  }
+
+  return previous;
+}
