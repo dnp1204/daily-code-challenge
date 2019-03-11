1d7e553641177a43fc387f3fb00e1a4b3f047397
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-11 14:04:35 -0400

    solve problem 26

--- 21-40/problem-26.js
+++ 21-40/problem-26.js
@@ -10,4 +10,31 @@
  *
  * Do this in constant space and in one pass.
  */
-// TODO:
+const traverse = (head, n) => {
+  let node = head;
+  for (let i = 1; i < n; i++) {
+    node = node.next;
+  }
+  return node;
+};
+
+var removeNthFromEnd = function(head, n) {
+  let left = head;
+  let right = traverse(head, n);
+
+  // if already reach the end, return directly
+  if (right.next === null) {
+    return head.next;
+  }
+  right = right.next;
+
+  // keep pointer to previous of element to be deleted
+  while (right.next !== null) {
+    left = left.next;
+    right = right.next;
+  }
+  // remove that element
+  let next_node = left.next.next;
+  left.next = next_node;
+  return head;
+};
