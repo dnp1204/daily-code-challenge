5de8bdf4f8db619e3771c11a50495d3850476e30
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-11 16:32:03 -0400

    start problem 22

--- 21-40/problem-22.js
+++ 21-40/problem-22.js
@@ -1,10 +1,14 @@
 /**
  * Company: Google.
  *
- * A knight's tour is a sequence of moves by a knight on a chessboard such that all
- * squares are visited once.
+ * A knight's tour is a sequence of moves by a knight on a chessboard such that
+ * all squares are visited once.
  *
  * Given N, write a function to return the number of knight's tours on an N by N
  * chessboard.
+ *
+ * Dailycoding: https://www.dailycodingproblem.com/blog/knights-tour/
+ * GeeksForGeeks: https://www.geeksforgeeks.org/the-knights-tour-problem-backtracking-1/
  */
 // TODO:
+function knightsTours(n) {}
