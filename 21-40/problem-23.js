37c67f360ad6e62d81c888785bba6036219d8441
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-11 16:25:17 -0400

    solve problem 23

--- 21-40/problem-23.js
+++ 21-40/problem-23.js
@@ -8,5 +8,77 @@
  * Implement an efficient sudoku solver.
  *
  * Leetcode: https://leetcode.com/problems/sudoku-solver/
+ * Yotube: https://www.youtube.com/watch?v=JzONv5kaPJM
  */
-// TODO:
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var solveSudoku = function(board) {
+  const canPlaceValue = function(board, row, col, charToPlace) {
+    for (const element of board) {
+      if (charToPlace === element[col]) {
+        return false;
+      }
+    }
+
+    for (let i = 0; i < board.length; i++) {
+      if (charToPlace === board[row][i]) {
+        return false;
+      }
+    }
+
+    const regionSize = Math.floor(Math.sqrt(board.length));
+
+    const I = Math.floor(row / regionSize);
+    const J = Math.floor(col / regionSize);
+
+    const topLeftOfSubBoxRow = regionSize * I;
+    const topLeftOfSubBoxCol = regionSize * J;
+
+    for (let i = 0; i < regionSize; i++) {
+      for (let j = 0; j < regionSize; j++) {
+        if (
+          charToPlace == board[topLeftOfSubBoxRow + i][topLeftOfSubBoxCol + j]
+        ) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  };
+
+  const solveSudokuCell = function(row, col, board) {
+    if (col === board[row].length) {
+      col = 0;
+      row++;
+
+      // Entire board has been filled without conflict.
+      if (row === board.length) {
+        return true;
+      }
+    }
+
+    if (board[row][col] !== '.') {
+      return solveSudokuCell(row, col + 1, board);
+    }
+
+    for (let value = 1; value <= board.length; value++) {
+      if (canPlaceValue(board, row, col, value + '')) {
+        board[row][col] = value + '';
+        if (solveSudokuCell(row, col + 1, board)) {
+          return true;
+        }
+      }
+    }
+
+    // Undo assignment to this cell. No values worked in it meaning that
+    // previous states put us in a position we cannot solve from. Hence,
+    // we backtrack by returning "false" to our caller.
+    board[row][col] = '.';
+    return false;
+  };
+
+  solveSudokuCell(0, 0, board);
+};
