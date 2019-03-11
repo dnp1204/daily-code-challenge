ac65e4e13f7c1baacd1127f8d1c54faa83287515
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-11 15:44:33 -0400

    start problem 23

--- 21-40/problem-22.js
+++ 21-40/problem-22.js
@@ -1,9 +1,10 @@
 /**
- * Company: Google
- * 
- * A knight's tour is a sequence of moves by a knight on a chessboard such that all 
+ * Company: Google.
+ *
+ * A knight's tour is a sequence of moves by a knight on a chessboard such that all
  * squares are visited once.
- * 
- * Given N, write a function to return the number of knight's tours on an N by N chessboard.
+ *
+ * Given N, write a function to return the number of knight's tours on an N by N
+ * chessboard.
  */
 // TODO:
