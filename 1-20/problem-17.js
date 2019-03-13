2d0b0bc0e5f68133ae025e0cd6c6c7a55ac8e5a7
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-13 16:42:19 -0400

    solve problem 17

--- 1-20/problem-17.js
+++ 1-20/problem-17.js
@@ -1,5 +1,5 @@
 /**
- * Company: Facebook
+ * Company: Facebook.
  *
  * Given a array of numbers representing the stock prices of a company in
  * chronological order, write a function that calculates the maximum profit
@@ -8,5 +8,23 @@
  *
  * For example, given [9, 11, 8, 5, 7, 10], you should return 5, since you could
  * buy the stock at 5 dollars and sell it at 10 dollars.
+ *
+ * Leetcode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
  */
-// TODO:
+var maxProfit = function(prices) {
+  if (!prices.length) return 0;
+  let maxStart = (maxEnd = currentStart = 0);
+  let max = prices[maxEnd] - prices[maxStart];
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] - prices[currentStart] < 0) {
+      currentStart = i;
+    } else if (prices[i] - prices[currentStart] > max) {
+      max = prices[i] - prices[currentStart];
+      maxStart = currentStart;
+      maxEnd = i;
+    }
+  }
+
+  return { max, maxStart, maxEnd };
+};
