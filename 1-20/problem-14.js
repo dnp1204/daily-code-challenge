752cd145fbe3ead1e617f47be99393cae7348bdc
Doanh Pham <doanhpham1995@gmail.com>
2019-03-13 18:36:53 -0400

    start problem 14

--- 1-20/problem-14.js
+++ 1-20/problem-14.js
@@ -1,12 +1,14 @@
 /**
- * Company: Facebook
- * 
- * A builder is looking to build a row of N houses that can be of 
- * K different colors. He has a goal of minimizing cost while ensuring 
+ * Company: Facebook.
+ *
+ * A builder is looking to build a row of N houses that can be of
+ * K different colors. He has a goal of minimizing cost while ensuring
  * that no two neighboring houses are of the same color.
- * 
- * Given an N by K matrix where the nth row and kth column represents 
- * the cost to build the nth house with kth color, return the minimum 
+ *
+ * Given an N by K matrix where the nth row and kth column represents
+ * the cost to build the nth house with kth color, return the minimum
  * cost which achieves this goal.
+ *
+ * Leetcode: https://leetcode.com/problems/paint-house/
  */
 // TODO:
