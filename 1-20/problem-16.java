0d716cb49b620f5e87374d8efff9f9c6ba7a31a6
Doanh Pham <doanhpham1995@gmail.com>
2019-03-13 18:28:17 -0400

    add youtube link

--- 1-20/problem-16.java
+++ 1-20/problem-16.java
@@ -1,51 +1,45 @@
 /**
  * Company: Microsoft.
  *
- * Compute the running median of a sequence of numbers. That
- * is, given a stream of numbers, print out the median of the
- * list so far on each new element.
+ * Compute the running median of a sequence of numbers. That is, given a stream
+ * of numbers, print out the median of the list so far on each new element.
  *
- * Recall that the median of an even-numbered list is the average
- * of the two middle numbers.
+ * Recall that the median of an even-numbered list is the average of the two
+ * middle numbers.
  *
- * For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your
- * algorithm should print out:
- * 2
- * 1.5
- * 2
- * 3.5
- * 2
- * 2
- * 2
+ * For example, given the sequence [2, 1, 5, 7, 2, 0, 5], your algorithm should
+ * print out: 2 1.5 2 3.5 2 2 2
  *
  * Leetcode: https://leetcode.com/problems/find-median-from-data-stream/
+ * Youtube:
+ * https://www.youtube.com/watch?v=1CxyVdA_654&feature=youtu.be&app=desktop
  */
 class MedianFinder {
-  private PriorityQueue<Integer> small;
-  private PriorityQueue<Integer> large;
-  private boolean even;
-  
-  public MedianFinder() {
-      small = new PriorityQueue<>(Collections.reverseOrder());
-      large = new PriorityQueue<>();
-      even = true;
-  }
-  
-  public void addNum(int num) {
-     if (even) {
-          large.offer(num);
-          small.offer(large.poll());
-      } else {
-          small.offer(num);
-          large.offer(small.poll());
-      }
-      even = !even;
-  }
-  
-  public double findMedian() {
-      if (even)
-          return (small.peek() + large.peek()) / 2.0;
-      else
-          return small.peek();
-  }
+    private PriorityQueue<Integer> small;
+    private PriorityQueue<Integer> large;
+    private boolean even;
+
+    public MedianFinder() {
+        small = new PriorityQueue<>(Collections.reverseOrder());
+        large = new PriorityQueue<>();
+        even = true;
+    }
+
+    public void addNum(int num) {
+        if (even) {
+            large.offer(num);
+            small.offer(large.poll());
+        } else {
+            small.offer(num);
+            large.offer(small.poll());
+        }
+        even = !even;
+    }
+
+    public double findMedian() {
+        if (even)
+            return (small.peek() + large.peek()) / 2.0;
+        else
+            return small.peek();
+    }
 }
