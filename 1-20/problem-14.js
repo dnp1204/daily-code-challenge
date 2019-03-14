de7eefa570b67d1841922e0a7e16b94e9596ac52
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-14 15:01:29 -0400

    solve problem 14

--- 1-20/problem-14.js
+++ 1-20/problem-14.js
@@ -16,7 +16,55 @@ function findMinCosts(costs) {
 
   const numHouses = costs.length;
   const numColors = costs[0].length;
+  const table = [];
+
+  let min = Number.MAX_SAFE_INTEGER;
+  let minPath = [];
+
+  for (let i = 0; i < numHouses; i++) {
+    table[i] = [];
+
+    for (let j = 0; j < numColors; j++) {
+      table[i][j] = Number.MAX_SAFE_INTEGER;
+    }
+  }
+
+  const helper = function(atHouse, cost, previousColor, path = [], memo) {
+    if (atHouse === numHouses) {
+      min = Math.min(min, cost);
+      minPath = min === cost ? path : minPath;
+      return cost;
+    }
+
+    if (
+      memo[atHouse][previousColor] &&
+      memo[atHouse][previousColor] !== Number.MAX_SAFE_INTEGER &&
+      memo[atHouse][previousColor] < cost
+    ) {
+      return memo[atHouse][previousColor];
+    }
+
+    for (let i = 0; i < numColors; i++) {
+      if (previousColor !== i) {
+        const result = helper(
+          atHouse + 1,
+          cost + costs[atHouse][i],
+          i,
+          [...path, i],
+          memo
+        );
+        memo[atHouse][i] = result;
+      }
+    }
+
+    return cost;
+  };
+
+  helper(0, 0, -1, [], table);
+  return min;
 }
 
-const costs = [[4, 0, 3], [8, 3, 8], [4, 5, 0], [3, 4, 4], [8, 8, 0]];
-console.log(findMinCosts(costs));
+console.log(
+  findMinCosts([[4, 0, 3], [8, 3, 8], [4, 5, 0], [3, 4, 4], [8, 8, 0]])
+);
+console.log(findMinCosts([[14, 2, 11], [11, 14, 5], [14, 3, 10]]));
