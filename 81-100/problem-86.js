963ffb057a3e9cdd7e194ff8a6c59974b5ef482f
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-18 19:55:18 -0400

    solve problem 86

--- 81-100/problem-86.js
+++ 81-100/problem-86.js
@@ -12,6 +12,31 @@
  *
  * If [a, b] is one solution with a <= b, and [c, d] is another solution with c <= d, then
  * [a, b] < [c, d]
- * If a < c OR a==c AND b < d.
+ * If a < c OR a == c AND b < d.
  */
-// TODO:
+function findPrimePair(n) {
+  const isPrime = Array(n + 1);
+
+  const SieveOfEratosthenes = function(n, isPrime) {
+    isPrime[0] = isPrime[1] = false;
+    for (let i = 2; i <= n; i++) isPrime[i] = true;
+
+    for (let p = 2; p * p <= n; p++) {
+      if (isPrime[p] == true) {
+        for (let i = p * 2; i <= n; i += p) isPrime[i] = false;
+      }
+    }
+    return false;
+  };
+
+  SieveOfEratosthenes(n, isPrime);
+  for (let i = 0; i < n; i++) {
+    if (isPrime[i] && isPrime[n - i]) {
+      console.log(i + ' ' + (n - i));
+      return;
+    }
+  }
+}
+
+findPrimePair(74);
+findPrimePair(24);
