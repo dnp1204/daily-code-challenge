a79b5884c9e7e1c176539ced0a7cc99ca8267c0c
Doanh Pham <doanhpham1995@gmail.com>
2019-03-17 23:06:49 -0400

    solve problem 5

--- 1-20/problem-5.js
+++ 1-20/problem-5.js
@@ -11,5 +11,29 @@
  *
  * Leetcode: https://leetcode.com/problems/maximum-product-of-three-numbers/
  */
-// TODO:
-[1, null, null, null, null, 1];
+var maximumProduct = function(nums) {
+  let max1 = (max2 = max3 = Number.MIN_SAFE_INTEGER);
+  let min1 = (min2 = Number.MAX_SAFE_INTEGER);
+
+  for (const num of nums) {
+    if (max1 < num) {
+      max3 = max2;
+      max2 = max1;
+      max1 = num;
+    } else if (max2 < num) {
+      max3 = max2;
+      max2 = num;
+    } else if (max3 < num) {
+      max3 = num;
+    }
+
+    if (min1 > num) {
+      min2 = min1;
+      min1 = num;
+    } else if (min2 > num) {
+      min2 = num;
+    }
+  }
+
+  return Math.max(max1 * max2 * max3, max1 * min1 * min2);
+};
