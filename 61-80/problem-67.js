82c403e56a1819eaad694b63a1ff0c3d9acb2cc5
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-20 10:45:03 -0400

    solve problem 67

--- 61-80/problem-67.js
+++ 61-80/problem-67.js
@@ -1,7 +1,35 @@
 /**
  * Company: Google.
  *
- * Given an integer n and a list of integers l, write a function that randomly generates
- * a number from 0 to n-1 that isn't in l (uniform).
+ * Given an integer n and a list of integers l, write a function that
+ * randomly generates a number from 0 to n-1 that isn't in l (uniform).
  */
-// TODO:
+const getRandNotInList = (n, list) => {
+  const picks = [];
+  const getPickable = () => {
+    let i = 0;
+    let dir = Math.sign(n);
+    list = new Set(list);
+
+    while (i !== n) {
+      if (!list.has(i)) {
+        picks.push(i);
+      }
+      i += dir;
+    }
+  };
+
+  n = Math.floor(n);
+  if (n === 0) {
+    return list.includes(0) ? undefined : 0;
+  }
+
+  getPickable();
+
+  // (| 0) same as Math.floor (only for positive ints).
+  return picks[(Math.random() * picks.length) | 0];
+};
+
+for (let i = 0; i < 20; i++) {
+  console.log(getRandNotInList(-10, [1, 4, 5, 6]));
+}
