e4b8326590fe1385d56de59a109f88e682d97867
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-21 17:18:28 -0400

    solve problem 20

--- 1-20/problem-20.js
+++ 1-20/problem-20.js
@@ -16,4 +16,34 @@
  *
  * Leetcode: https://leetcode.com/problems/trapping-rain-water/
  */
-// TODO:
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+  let left = 0;
+  let right = height.length - 1;
+  let leftMax = 0;
+  let rightMax = 0;
+  let max = 0;
+
+  while (left <= right) {
+    leftMax = Math.max(leftMax, height[left]);
+    rightMax = Math.max(rightMax, height[right]);
+
+    if (leftMax < rightMax) {
+      max += leftMax - height[left];
+      left++;
+    } else {
+      max += rightMax - height[right];
+      right--;
+    }
+  }
+
+  return max;
+};
+
+console.log(trap([2, 1, 2]));
+console.log(trap([3, 0, 1, 3, 0, 5]));
+console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+console.log(trap([0, 1, 0, 2, 1, 0, 4]));
