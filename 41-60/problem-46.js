1605539725f8dff3fbd3a000ca032181ecc82010
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-22 13:56:55 -0400

    solve problem 46

--- 41-60/problem-46.js
+++ 41-60/problem-46.js
@@ -9,4 +9,57 @@
  *
  * Leetcode: https://leetcode.com/problems/remove-invalid-parentheses/
  */
-// TODO:
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var removeInvalidParentheses = function(s) {
+  if (s == null) return res;
+  let res = [];
+
+  const visited = new Set();
+  const queue = [];
+  let found = false;
+
+  queue.push(s);
+  visited.add(s);
+
+  while (queue.length) {
+    s = queue.shift();
+
+    if (isValid(s)) {
+      res.push(s);
+      found = true;
+    }
+
+    if (found) continue;
+
+    for (let i = 0; i < s.length; i++) {
+      if (s.charAt(i) !== '(' && s.charAt(i) !== ')') continue;
+
+      const t = s.substring(0, i) + s.substring(i + 1);
+
+      if (!visited.has(t)) {
+        queue.push(t);
+        visited.add(t);
+      }
+    }
+  }
+
+  return res;
+};
+
+function isValid(s) {
+  let count = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const c = s.charAt(i);
+    if (c == '(') count++;
+    if (c == ')' && count-- === 0) return false;
+  }
+
+  return count == 0;
+}
+
+console.log(removeInvalidParentheses('()())()'));
+console.log(removeInvalidParentheses(')('));
