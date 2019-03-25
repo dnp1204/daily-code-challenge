160678700a85202af05b433a6c26851d66a4f8f6
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-03-25 16:10:15 -0400

    solve problem 47

--- 41-60/problem-47.js
+++ 41-60/problem-47.js
@@ -18,4 +18,47 @@
  * Leetcode: https://leetcode.com/problems/regular-expression-matching/
  * Youtube: https://www.youtube.com/watch?v=l3hda49XcDE
  */
-// TODO:
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+  const table = [];
+  for (let i = 0; i <= s.length; i++) {
+    table[i] = [];
+    for (let j = 0; j <= p.length; j++) {
+      table[i][j] = false;
+    }
+  }
+  table[0][0] = true;
+
+  for (let j = 1; j < table[0].length; j++) {
+    if (p.charAt(j - 1) === '*') {
+      if (j === 1) {
+        table[0][j] = true;
+      } else {
+        table[0][j] = table[0][j - 2];
+      }
+    }
+  }
+
+  for (let i = 1; i < table.length; i++) {
+    for (let j = 1; j < table[0].length; j++) {
+      if (s.charAt(i - 1) === p.charAt(j - 1) || p.charAt(j - 1) === '.') {
+        table[i][j] = table[i - 1][j - 1];
+      } else if (p.charAt(j - 1) === '*') {
+        if (table[i][j - 2]) {
+          table[i][j] = true;
+        } else if (
+          s.charAt(i - 1) === p.charAt(j - 2) ||
+          p.charAt(j - 2) === '.'
+        ) {
+          table[i][j] = table[i - 1][j];
+        }
+      }
+    }
+  }
+
+  return table[s.length][p.length];
+};
