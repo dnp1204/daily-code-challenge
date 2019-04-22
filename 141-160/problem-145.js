fbac28f1d2653e2f8f788058c15cd009f9bba098
Doanh Pham <doanhpham1995@gmail.com>
2019-04-22 18:35:16 -0400

    solve problem 145

--- 141-160/problem-145.js
+++ 141-160/problem-145.js
@@ -10,4 +10,32 @@
  * Leetcode: https://leetcode.com/problems/palindrome-partitioning-ii/
  * Youtube: https://www.youtube.com/watch?v=lDYIvtBVmgo
  */
-// TODO:
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minCut = function(s) {
+  const n = s.length;
+  const cut = Array(n);
+  const pal = [];
+
+  for (let i = 0; i < n; i++) {
+    pal[i] = [];
+    for (let j = 0; j < n; j++) {
+      pal[i][j] = false;
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    let min = i;
+    for (let j = 0; j <= i; j++) {
+      if (s.charAt(i) === s.charAt(j) && (j + 1 > i - 1 || pal[j + 1][i - 1])) {
+        pal[j][i] = true;
+        min = j === 0 ? 0 : Math.min(min, cut[j - 1] + 1);
+      }
+    }
+    cut[i] = min;
+  }
+
+  return cut[n - 1];
+};
