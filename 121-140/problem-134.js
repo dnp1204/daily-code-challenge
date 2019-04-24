462ebb4aa48b508067b59ffc027579014374222f
Doanh Pham <doanhpham1995@gmail.com>
2019-04-23 21:45:04 -0400

    solve problem 134

--- 121-140/problem-134.js
+++ 121-140/problem-134.js
@@ -13,6 +13,152 @@
  * Given start = "dog", end = "cat", and dictionary = {"dot", "tod", "dat", "dar"},
  * return null as there is no possible transformation from dog to cat.
  *
- * Leetcode: https://leetcode.com/problems/word-ladder-ii/
+ * Leetcode: https://leetcode.com/problems/word-ladder/ and https://leetcode.com/problems/word-ladder-ii/
  */
-// TODO:
+const { Graph } = require('../libraries/Graph');
+
+var findLadders = function(beginWord, endWord, wordList) {
+  const graph = new Graph();
+  const newWordList = [beginWord, ...wordList];
+
+  const isExist = wordList.find(word => word === endWord);
+  if (!isExist) return 0;
+
+  for (let i = 0; i < newWordList.length; i++) {
+    graph.addVertex(newWordList[i]);
+  }
+
+  for (let i = 0; i < newWordList.length; i++) {
+    const word1 = newWordList[i];
+
+    for (let j = i + 1; j < newWordList.length; j++) {
+      const word2 = newWordList[j];
+
+      if (isValid(word1, word2)) {
+        const node1 = graph.vertices[word1];
+        const node2 = graph.vertices[word2];
+        graph.addEdge(node1, node2, 1);
+      }
+    }
+  }
+
+  return graph.dijkstraAll(beginWord, endWord);
+};
+
+const isValid = function(word1, word2) {
+  let index = 0;
+  let count = 0;
+  while (index < word1.length) {
+    if (word1.charAt(index) !== word2.charAt(index)) count++;
+    if (count > 1) return false;
+    index++;
+  }
+  return true;
+};
+
+console.log(
+  findLadders('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog'])
+);
+console.log(findLadders('a', 'c', ['a', 'b', 'c']));
+console.log(
+  findLadders('lost', 'cost', ['most', 'fist', 'lost', 'cost', 'fish'])
+);
+console.log(
+  findLadders('qa', 'sq', [
+    'si',
+    'go',
+    'se',
+    'cm',
+    'so',
+    'ph',
+    'mt',
+    'db',
+    'mb',
+    'sb',
+    'kr',
+    'ln',
+    'tm',
+    'le',
+    'av',
+    'sm',
+    'ar',
+    'ci',
+    'ca',
+    'br',
+    'ti',
+    'ba',
+    'to',
+    'ra',
+    'fa',
+    'yo',
+    'ow',
+    'sn',
+    'ya',
+    'cr',
+    'po',
+    'fe',
+    'ho',
+    'ma',
+    're',
+    'or',
+    'rn',
+    'au',
+    'ur',
+    'rh',
+    'sr',
+    'tc',
+    'lt',
+    'lo',
+    'as',
+    'fr',
+    'nb',
+    'yb',
+    'if',
+    'pb',
+    'ge',
+    'th',
+    'pm',
+    'rb',
+    'sh',
+    'co',
+    'ga',
+    'li',
+    'ha',
+    'hz',
+    'no',
+    'bi',
+    'di',
+    'hi',
+    'qa',
+    'pi',
+    'os',
+    'uh',
+    'wm',
+    'an',
+    'me',
+    'mo',
+    'na',
+    'la',
+    'st',
+    'er',
+    'sc',
+    'ne',
+    'mn',
+    'mi',
+    'am',
+    'ex',
+    'pt',
+    'io',
+    'be',
+    'fm',
+    'ta',
+    'tb',
+    'ni',
+    'mr',
+    'pa',
+    'he',
+    'lr',
+    'sq',
+    'ye'
+  ])
+);
---
+++ libraries/Graph.js
@@ -0,0 +1,180 @@
+const PriorityQueue = require('./PriorityQueue');
+
+class Node {
+  constructor(node, weight) {
+    this.node = node;
+    this.weight = weight;
+  }
+}
+
+class Vertex {
+  constructor(val) {
+    this.val = val;
+    this.neighbors = [];
+  }
+
+  addNeighbor(node, weight) {
+    const isExisted = this.neighbors.find(element => {
+      if (element.val === node.val) {
+        console.log('Updated weight for duplicated node');
+        element.weight = weight;
+        return true;
+      }
+
+      return false;
+    });
+
+    if (!isExisted) {
+      const neighbor = new Node(node, weight);
+      this.neighbors.push(neighbor);
+    }
+  }
+}
+
+class Graph {
+  constructor() {
+    this.vertices = {};
+  }
+
+  addVertex(val) {
+    let node = null;
+    if (!this.vertices[val]) {
+      node = new Vertex(val);
+      this.vertices[val] = node;
+    } else {
+      node = this.vertices[val];
+    }
+    return node;
+  }
+
+  addEdge(node1, node2, weight = 0) {
+    if (!node1 || !node2 || !node1.val || !node2.val)
+      throw new Error('Invalid graph node');
+    node1.addNeighbor(node2, weight);
+    node2.addNeighbor(node1, weight);
+  }
+
+  dijkstraAllHelper(map, key, path, result) {
+    if (!map[key]) {
+      if (path.length > 1) {
+        const newPath = path.reverse();
+        result.push(newPath);
+      }
+      return result;
+    }
+
+    const iterator = map[key][Symbol.iterator]();
+    let element = iterator.next();
+    while (!element.done) {
+      this.dijkstraAllHelper(
+        map,
+        element.value,
+        [...path, element.value],
+        result
+      );
+      element = iterator.next();
+    }
+
+    return result;
+  }
+
+  dijkstraAll(start, end) {
+    const nodes = new PriorityQueue(
+      [],
+      (node1, node2) => node1.distance - node2.distance
+    );
+    const previous = {};
+    const distances = {};
+
+    for (const key of Object.keys(this.vertices)) {
+      previous[key] = null;
+      if (key === start) {
+        nodes.push({ val: key, distance: 0 });
+        distances[key] = 0;
+      } else {
+        nodes.push({ val: key, distance: Infinity });
+        distances[key] = Infinity;
+      }
+    }
+
+    while (nodes.data.length) {
+      const smallest = nodes.pop();
+      const node = this.vertices[smallest.val];
+
+      if (smallest.val === end && distances[end] !== Infinity) {
+        continue;
+      } else {
+        if (smallest && distances[smallest.val] !== Infinity) {
+          for (const neighbor of node.neighbors) {
+            const val = neighbor.node.val;
+            const candidate = distances[smallest.val] + neighbor.weight;
+
+            if (candidate < distances[val]) {
+              previous[val] = new Set([smallest.val]);
+              distances[val] = candidate;
+              nodes.push({ val, distance: candidate });
+            } else if (candidate === distances[val]) {
+              previous[val].add(smallest.val);
+            }
+          }
+        }
+      }
+    }
+
+    return this.dijkstraAllHelper(previous, end, [end], []);
+  }
+
+  dijkstra(start, end) {
+    const nodes = new PriorityQueue(
+      [],
+      (node1, node2) => node1.distance - node2.distance
+    );
+    const previous = {};
+    const distances = {};
+
+    for (const key of Object.keys(this.vertices)) {
+      previous[key] = null;
+      if (key === start) {
+        nodes.push({ val: key, distance: 0 });
+        distances[key] = 0;
+      } else {
+        nodes.push({ val: key, distance: Infinity });
+        distances[key] = Infinity;
+      }
+    }
+
+    while (nodes.data.length) {
+      const path = [];
+      const smallest = nodes.pop();
+      const node = this.vertices[smallest.val];
+
+      if (smallest.val === end && distances[end] !== Infinity) {
+        let current = previous[end];
+        path.push(end);
+        while (previous[current]) {
+          path.push(current);
+          current = previous[current];
+        }
+        path.push(start);
+        return path.reverse();
+      } else {
+        if (smallest && distances[smallest.val] !== Infinity) {
+          for (const neighbor of node.neighbors) {
+            const val = neighbor.node.val;
+            const candidate = distances[smallest.val] + neighbor.weight;
+
+            if (candidate < distances[val]) {
+              previous[val] = smallest.val;
+              distances[val] = candidate;
+              nodes.push({ val, distance: candidate });
+            }
+          }
+        }
+      }
+    }
+
+    return null;
+  }
+}
+
+module.exports = { Graph };
