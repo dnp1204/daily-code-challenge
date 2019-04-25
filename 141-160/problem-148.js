18933fece0497550c8130f8938ae1350745580ee
Doanh Pham <doanhpham1995@gmail.com>
2019-04-24 21:09:07 -0400

    solve problem 148

--- 141-160/problem-148.js
+++ 141-160/problem-148.js
@@ -5,4 +5,25 @@
  *
  * For example, given the numbers [42, 56, 14], return 14.
  */
-// TODO:
+const gcd = function(a, b) {
+  if (a === 0) return b;
+  if (b === 0) return a;
+  if (a === b) return a;
+  if (a > b) return gcd(a - b, b);
+  return gcd(a, b - a);
+};
+
+const findGCD = function(nums) {
+  if (!nums.length) return -1;
+  let result = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    result = gcd(result, nums[i]);
+  }
+  return result;
+};
+
+console.log(findGCD([1, 2, 3]));
+console.log(findGCD([5, 5, 7]));
+console.log(findGCD([3, 6, 10]));
+console.log(findGCD([42, 56, 14]));
+console.log(findGCD([2, 4, 6, 8]));
