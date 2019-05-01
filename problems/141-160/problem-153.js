8963edddbda02b7605cfc5b4f4db20dd73dd81d2
Doanh Pham <doanhpham1995@gmail.com>
2019-05-01 05:29:13 -0400

    solve problem 153

--- problems/141-160/problem-153.js
+++ problems/141-160/problem-153.js
@@ -7,4 +7,25 @@
  * For example, given the array [5, 1, 3, 5, 2, 3, 4, 1], return 5 as the longest subarray of
  * distinct elements is [5, 2, 3, 4, 1].
  */
-// TODO:
+const findLongestDistinct = function(nums) {
+  let visited = {};
+  let start = 0,
+    current = 0,
+    max = 0;
+
+  while (current < nums.length) {
+    visited = {};
+    while (!visited[nums[current]] && current < nums.length) {
+      visited[nums[current]] = true;
+      current++;
+    }
+    max = Math.max(max, current - start);
+    start = current;
+  }
+
+  return max;
+};
+
+console.log(findLongestDistinct([5, 1, 3, 5, 2, 3, 4, 1]));
+console.log(findLongestDistinct([1, 2, 3, 4, 2, 3]));
+console.log(findLongestDistinct([1, 1, 1, 1, 1, 1]));
