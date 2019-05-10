9e68af1e56cebdbda4de795873447a680fc7b163
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-05-10 13:55:56 -0400

    solve problem 163

--- problems/161-180/problem-163.js
+++ problems/161-180/problem-163.js
@@ -10,4 +10,34 @@
  *
  * Leetcode: https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
  */
-// TODO:
+var minAddToMakeValid = function(S) {
+  const stack = [];
+  const ans = [];
+
+  for (let i = 0; i < S.length; i++) {
+    if (S.charAt(i) === '(') {
+      stack.push('(');
+      ans.push('(');
+    } else {
+      if (stack.length) {
+        stack.pop();
+      } else {
+        ans.push('(');
+      }
+      ans.push(')');
+    }
+  }
+
+  while (stack.length) {
+    stack.pop();
+    ans.push(')');
+  }
+
+  return ans.join('');
+};
+
+console.log(minAddToMakeValid('(()'));
+console.log(minAddToMakeValid('(((((((()'));
+console.log(minAddToMakeValid('))()('));
+console.log(minAddToMakeValid('()()()()'));
+console.log(minAddToMakeValid('(())'));
