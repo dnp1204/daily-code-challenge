79bbc3a2bd717e8c431f9dea31b1565fb71f2164
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-05-10 14:19:49 -0400

    solve problem 164

--- problems/161-180/problem-164.js
+++ problems/161-180/problem-164.js
@@ -5,7 +5,29 @@
  * P "stabs" X if every interval in X contains at least one point in P. Compute
  * the smallest set of points that stabs X.
  *
- * For example, given the intervals [(1, 4), (4, 5), (7, 9), (9, 12)], you should
+ * For example, given the intervals [[1, 4), (4, 5), (7, 9), (9, 12)], you should
  * return [4, 9].
  */
-// TODO:
+const findPointsP = function(intervals) {
+  const ans = [];
+
+  if (!intervals.length) return ans;
+
+  intervals.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    return a[1] - b[1];
+  });
+
+  ans.push(intervals[0][1]);
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] > ans[ans.length - 1]) {
+      ans.push(intervals[i][1]);
+    }
+  }
+
+  return ans;
+};
+
+console.log(findPointsP([[1, 4], [4, 5], [7, 9], [9, 12]]));
+console.log(findPointsP([[1, 4], [-2, 0], [4, 5], [7, 9]]));
+console.log(findPointsP([[0, 4], [-2, 0], [4, 5], [7, 9]]));
