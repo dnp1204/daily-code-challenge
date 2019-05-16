41f6a3f25d60c49372e664993a96310a196c34fd
Doanh Pham <doanh.pham@retailbusinessservices.com>
2019-05-16 14:40:41 -0400

    solve problem 170

--- problems/161-180/problem-170.js
+++ problems/161-180/problem-170.js
@@ -8,4 +8,22 @@
  * Given an array and a permutation, apply the permutation to the array. For example, given
  * the array ["a", "b", "c"] and the permutation [2, 1, 0], return ["c", "b", "a"].
  */
-// TODO:
+const permutate = function(arr, index) {
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    while (index[i] !== i) {
+      swap(arr, index[i], i);
+      swap(index, index[i], i);
+    }
+  }
+};
+
+function swap(arr, i, j) {
+  const temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+permutate(['a', 'b', 'c'], [2, 1, 0]);
+permutate(['a', 'b', 'c'], [2, 0, 1]);
+permutate(['a', 'b', 'c', 'd'], [3, 0, 1, 2]);
