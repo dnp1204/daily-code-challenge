3419a88bbbdcde91096bf034c3472abbe75381b3
Doanh Pham <doanhpham1995@gmail.com>
2019-05-20 20:55:12 -0400

    solve problem 171

--- problems/161-180/problem-171.js
+++ problems/161-180/problem-171.js
@@ -7,4 +7,31 @@
  *
  * Leetcode: https://leetcode.com/problems/is-graph-bipartite/
  */
-// TODO:
+var isBipartite = function(graph) {
+  const n = graph.length;
+  const BLUE = 0,
+    YELLOW = 1;
+  const colors = Array(n).fill(-1);
+
+  const dfs = function(index, color) {
+    if (colors[index] !== -1) return colors[index] !== color;
+    color === BLUE ? (colors[index] = YELLOW) : (colors[index] = BLUE);
+    const indexes = graph[index];
+    for (const i of indexes) {
+      if (!dfs(i, colors[index])) return false;
+    }
+    return true;
+  };
+
+  for (let i = 0; i < n; i++) {
+    const indexes = graph[i];
+    if (colors[i] === -1) {
+      colors[i] = BLUE;
+    }
+    for (const index of indexes) {
+      if (!dfs(index, colors[i])) return false;
+    }
+  }
+
+  return true;
+};
