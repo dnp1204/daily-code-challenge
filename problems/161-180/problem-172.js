d01078c11142d372caed768db659defe3c938277
Doanh Pham <doanhpham1995@gmail.com>
2019-05-20 20:58:00 -0400

    solve problem 173

--- problems/161-180/problem-172.js
+++ problems/161-180/problem-172.js
@@ -6,5 +6,43 @@
  *
  * For example, given the linked list 5 -> 1 -> 8 -> 0 -> 3 and k = 3, the solution
  * could be 1 -> 0 -> 5 -> 8 -> 3.
+ *
+ * Leetcode: https://leetcode.com/problems/partition-list/
  */
-// TODO:
+var partition = function(head, x) {
+  if (!head) return null;
+
+  let current = head,
+    start = head,
+    previous = null;
+
+  while (current) {
+    while (current && current.val >= x) {
+      previous = current;
+      current = current.next;
+    }
+
+    if (current && current != start) {
+      previous.next = current.next;
+      current.next = start.next;
+      start.next = current;
+      start = current;
+      current = previous;
+    }
+
+    if (!current) break;
+
+    previous = current;
+    current = current.next;
+  }
+
+  if (head.val >= x && start !== head) {
+    const newHead = head.next;
+    const next = start.next;
+    start.next = head;
+    head.next = next;
+    return newHead;
+  }
+
+  return head;
+};
