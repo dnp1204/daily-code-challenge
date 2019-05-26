3c8f515fd951e017ed9308e732961af46b0c8961
Doanh Pham <doanhpham1995@gmail.com>
2019-05-26 18:18:31 -0400

    solve problem 180

--- problems/161-180/problem-180.js
+++ problems/161-180/problem-180.js
@@ -5,3 +5,32 @@
  *
  * Leetcode: https://leetcode.com/problems/roman-to-integer/
  */
+var romanToInt = function(s) {
+  const value = {
+    M: 1000,
+    D: 500,
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1
+  };
+
+  let result = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s.charAt(i);
+    if (i < s.length - 1) {
+      const nextChar = s.charAt(i + 1);
+      if (value[nextChar] > value[char]) {
+        result += value[nextChar] - value[char];
+        i++;
+        continue;
+      }
+    }
+
+    result += value[char];
+  }
+
+  return result;
+};
