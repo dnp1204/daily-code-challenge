f06a7bbb95d07cbc39fcfa5b62aa504d546f2060
Doanh Pham <doanh.pham@vimeo.com>
2019-06-03 15:08:59 -0400

    solve problem 188

--- problems/100-199/problem-188.js
+++ problems/100-199/problem-188.js
@@ -6,5 +6,23 @@
  * For example, for the input [1, 2, 3, 10], you should return 7.
  *
  * Do this in O(N) time.
+ *
+ * GeeksForGeeks: https://www.geeksforgeeks.org/find-smallest-value-represented-sum-subset-given-array/
  */
 // TODO:
+const findMissingInteger = function(nums) {
+  let res = 1;
+
+  for (let i = 0; i < nums.length && nums[i] <= res; i++) {
+    res = res + nums[i];
+  }
+
+  return res;
+};
+
+console.log(findMissingInteger([1, 2, 3, 10]));
+console.log(findMissingInteger([1, 3, 6, 10, 11, 15]));
+console.log(findMissingInteger([1, 1, 1, 1]));
+console.log(findMissingInteger([1, 1, 3, 4]));
+console.log(findMissingInteger([1, 2, 5, 10, 20, 40]));
+console.log(findMissingInteger([1, 2, 3, 4, 5, 6]));
