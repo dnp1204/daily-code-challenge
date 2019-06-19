93148ea451e5c24dd369e22d98e99b1772974fcc
Doanh Pham <doanh.pham@vimeo.com>
2019-06-19 12:33:04 -0400

    solve problem 192

--- problems/100-199/problem-192.js
+++ problems/100-199/problem-192.js
@@ -3,5 +3,19 @@
  *
  * Given a list of numbers, create an algorithm that arranges them in order to form the largest possible
  * integer. For example, given [10, 7, 76, 415], you should return 77641510.
+ *
+ * Leetcode: https://leetcode.com/problems/largest-number/
  */
-// TODO:
+var largestNumber = function(nums) {
+  if (!nums.length) return '0';
+
+  nums.sort((a, b) => {
+    const s1 = '' + a + b;
+    const s2 = '' + b + a;
+    return s2 - s1;
+  });
+
+  const result = nums.join('');
+
+  return result[0] === '0' ? '0' : result;
+};
