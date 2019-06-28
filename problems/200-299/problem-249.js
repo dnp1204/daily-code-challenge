17063b49c7be33ec196e4c3ffa49b94b87e92550
Doanh Pham <doanhpham1995@gmail.com>
2019-06-28 19:38:19 -0400

    add leetcode link to problem 249

--- problems/200-299/problem-249.js
+++ problems/200-299/problem-249.js
@@ -2,5 +2,7 @@
  * Company: Salesforce.
  *
  * Given an array of integers, find the maximum XOR of any two elements.
+ *
+ * Leetcode: https://leetcode.com/problems/maximum-xor-of-two-numbers-in-an-array/
  */
 // TODO:
