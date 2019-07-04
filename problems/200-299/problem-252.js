1f33953c1ef68237d954ac1277aa154d8fc5402e
Doanh Pham <doanh.pham@vimeo.com>
2019-07-03 20:27:08 -0400

    solve problem 252

--- problems/200-299/problem-252.js
+++ problems/200-299/problem-252.js
@@ -6,4 +6,30 @@
  *
  * Create an algorithm to turn an ordinary fraction a / b, where a < b, into an Egyptian fraction.
  */
-// TODO:
+const findEgyptianFraction = function(a, b) {
+  if (a === 0 || b === 0) return;
+
+  if (b % a === 0) {
+    console.log(`1 / ${b / a}`);
+    return;
+  }
+
+  if (a % b === 0) {
+    console.log(`${b / a}`);
+    return;
+  }
+
+  if (a > b) {
+    console.log(Math.floor(a / b) + ' + ');
+    findEgyptianFraction(a % b, b);
+    return;
+  }
+
+  const n = Math.ceil(b / a);
+  console.log('1/' + n + ' + ');
+
+  findEgyptianFraction(a * n - b, b * n);
+};
+
+findEgyptianFraction(4, 13);
+findEgyptianFraction(13, 4);
