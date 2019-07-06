dc1582cdd0615af66a45c08a4aa14d1c5b5ba64e
Doanh Pham <doanhpham1995@gmail.com>
2019-07-06 12:58:31 -0400

    solve problem 257

--- problems/200-299/problem-257.js
+++ problems/200-299/problem-257.js
@@ -7,4 +7,40 @@
  *
  * Leetcode: https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
  */
-// TODO:
+var findUnsortedSubarray = function(nums) {
+  const n = nums.length;
+  let start = -1;
+  let end = -1;
+
+  let currentMax = nums[0];
+  for (let i = 0; i < n; i++) {
+    if (currentMax > nums[i]) end = i;
+    currentMax = Math.max(currentMax, nums[i]);
+  }
+
+  let currentMin = nums[n - 1];
+  for (let i = n - 1; i >= 0; i--) {
+    if (currentMin < nums[i]) start = i;
+    currentMin = Math.min(currentMin, nums[i]);
+  }
+
+  if (start === -1 || end === -1) return 0;
+
+  return end - start + 1;
+};
+
+const solution1 = function(nums) {
+  const sorted = [...nums].sort((a, b) => a - b);
+  const n = nums.length;
+
+  let start = n - 1,
+    end = 0;
+  for (let i = 0; i < n; i++) {
+    if (sorted[i] !== nums[i]) {
+      start = Math.min(start, i);
+      end = Math.max(end, i);
+    }
+  }
+
+  return start >= 0 && end > start ? end - start + 1 : 0;
+};
