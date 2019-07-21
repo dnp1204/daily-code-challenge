7e34df1b2c3a5afc82c29d246fe41086a1b1cad8
Doanh Pham <doanhpham1995@gmail.com>
2019-07-21 19:54:36 -0400

    solve problem 272

--- problems/200-299/problem-272.js
+++ problems/200-299/problem-272.js
@@ -6,4 +6,59 @@
  *
  * For example, throw_dice(3, 6, 7) should equal 15.
  */
-// TODO:
+const throwDice = function(N, faces, total) {
+  return method1(N, faces, total);
+};
+
+const method1 = function(N, faces, total) {
+  const memo = [];
+
+  for (let i = 0; i <= N; i++) {
+    memo[i] = [];
+    for (let j = 0; j <= total; j++) {
+      memo[i][j] = 0;
+    }
+  }
+
+  memo[0][0] = 1;
+
+  for (let i = 1; i <= N; i++) {
+    for (let j = i; j <= total; j++) {
+      memo[i][j] = memo[i][j - 1] + memo[i - 1][j - 1];
+      if (j - faces - 1 >= 0) {
+        memo[i][j] -= memo[i - 1][j - faces - 1];
+      }
+    }
+  }
+
+  return memo[N][total];
+};
+
+const method2 = function(N, faces, total) {
+  const memo = [];
+
+  for (let i = 0; i <= N; i++) {
+    memo[i] = [];
+    for (let j = 0; j <= total; j++) {
+      memo[i][j] = 0;
+    }
+  }
+
+  for (let i = 1; i <= faces && i <= total; i++) {
+    memo[1][i] = 1;
+  }
+
+  for (let i = 2; i <= N; i++) {
+    for (let j = 1; j <= total; j++) {
+      for (let k = 1; k <= faces && k < j; k++) {
+        memo[i][j] += memo[i - 1][j - k];
+      }
+    }
+  }
+
+  return memo[N][total];
+};
+
+console.log(throwDice(3, 6, 7));
+console.log(throwDice(3, 6, 12));
+console.log(throwDice(6, 3, 8));
