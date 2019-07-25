1fbb1f02a6f723d348998d44dfd05c2048be3c01
Doanh Pham <doanh.pham@vimeo.com>
2019-07-25 12:36:26 -0400

    solve problem 276

--- problems/200-299/problem-276.js
+++ problems/200-299/problem-276.js
@@ -10,4 +10,36 @@
  *
  * Leetcode: https://leetcode.com/problems/implement-strstr/
  */
-// TODO:
+var strStr = function(haystack, needle) {
+    if (!needle.length) return 0;
+    if (!haystack.length) return -1;
+    
+    const table = Array(needle.length).fill(0);
+    for (let i = 0, j = i + 1; j < needle.length; j++) {
+        if (needle.charAt(i) === needle.charAt(j)) {
+            table[j] = i + 1;
+            i++;
+        } else if (i !== 0) {
+            i = table[i - 1];
+            j--;
+        }
+    }
+    
+    let index1 = 0;
+    let index2 = 0;
+    
+    while (index1 < haystack.length && index2 < needle.length) {
+        if (haystack.charAt(index1) === needle.charAt(index2)) {
+            index1++;
+            index2++;
+        } else if (index2 !== 0) {
+            index2 = table[index2 - 1];
+        } else {
+            index1++;
+        }
+        
+        if (index2 === needle.length) return index1 - needle.length;
+    }
+    
+    return -1;
+};
