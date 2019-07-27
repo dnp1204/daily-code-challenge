5c3986d269a6a8ce1a40789ec4e53787e531fbaf
Doanh Pham <doanh.pham@vimeo.com>
2019-07-27 19:17:02 -0400

    solve problem 278

--- problems/200-299/problem-278.js
+++ problems/200-299/problem-278.js
@@ -5,4 +5,35 @@
  *
  * Leetcode: https://leetcode.com/problems/unique-binary-search-trees/
  */
-// TODO:
+var generateTrees = function(n) {
+  if (n === 0) return [];
+  const nums = [];
+  const result = [];
+
+  for (let i = 1; i <= n; i++) nums.push(i);
+
+  const helper = function(nums, memo = {}) {
+    if (nums.length === 0) return [null];
+    if (memo[nums.join(' ')]) return memo[nums.join(' ')];
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+      const lefts = helper(nums.slice(0, i), memo);
+      const rights = helper(nums.slice(i + 1), memo);
+      for (const left of lefts) {
+        for (const right of rights) {
+          const node = new TreeNode(nums[i]);
+          node.left = left;
+          node.right = right;
+          result.push(node);
+        }
+      }
+    }
+
+    memo[nums.join(' ')] = result;
+
+    return result;
+  };
+
+  return helper(nums);
+};
