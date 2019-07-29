fe3d6e42a903cb61286945416cce282d1f4d950e
Doanh Pham <doanh.pham@vimeo.com>
2019-07-29 12:29:41 -0400

    solve problem 280

--- problems/200-299/problem-280.js
+++ problems/200-299/problem-280.js
@@ -3,4 +3,39 @@
  *
  * Given an undirected graph, determine if it contains a cycle.
  */
-// TODO:
+const isCyclic = function(graph) {
+  const dfs = function(vertex, parent, visited) {
+    visited[vertex] = true;
+    for (const key of graph[vertex]) {
+      if (key === parent) continue;
+      if (visited[key] || dfs(key, vertex, visited)) return true;
+    }
+    return false;
+  };
+
+  const visited = {};
+  for (const key of Object.keys(graph)) {
+    if (visited[key]) continue;
+    if (dfs(key, null, visited)) return true;
+  }
+
+  return false;
+};
+
+let graph = {
+  '0': ['1', '2', '3'],
+  '1': ['0', '2'],
+  '2': ['0', '1'],
+  '3': ['0', '4'],
+  '4': ['3']
+};
+console.log(isCyclic(graph));
+
+graph = {
+  '0': ['1', '3'],
+  '1': ['0', '2'],
+  '2': ['1'],
+  '3': ['0', '4'],
+  '4': ['3']
+};
+console.log(isCyclic(graph));
