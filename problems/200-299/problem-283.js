730ae17e2cab2942c9623933cc090f81013e46f5
Doanh Pham <doanh.pham@vimeo.com>
2019-08-01 14:32:39 -0400

    solve problem 283

--- problems/200-299/problem-283.js
+++ problems/200-299/problem-283.js
@@ -8,5 +8,27 @@
  * according to the diatonic scale.
  *
  * Given an integer N, write a program that returns, in order, the first N regular numbers.
+ *
+ * Leetcode: https://leetcode.com/problems/ugly-number-ii/
  */
-// TODO:
+var nthUglyNumber = function(n) {
+  const ans = [];
+
+  let factor2 = 1,
+    factor3 = 1,
+    factor5 = 1;
+  let index1 = 0,
+    index3 = 0,
+    index5 = 0;
+
+  for (let i = 1; i <= n; i++) {
+    const min = Math.min(factor2, Math.min(factor3, factor5));
+    ans.push(min);
+
+    if (factor2 === min) factor2 = 2 * ans[index1++];
+    if (factor3 === min) factor3 = 3 * ans[index3++];
+    if (factor5 === min) factor5 = 5 * ans[index5++];
+  }
+
+  return ans;
+};
