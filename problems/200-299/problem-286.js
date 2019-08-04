3e03ffac61d5aaef0ef456699e7da3155ebb9b72
Doanh Pham <doanhpham1995@gmail.com>
2019-08-04 18:59:07 -0400

    add problem 286

---
+++ problems/200-299/problem-286.js
@@ -0,0 +1,15 @@
+/**
+ * Company: VMware.
+ *
+ * The skyline of a city is composed of several buildings of various widths and heights, possibly
+ * overlapping one another when viewed from a distance. We can represent the buildings using an
+ * array of (left, right, height) tuples, which tell us where on an imaginary x-axis a building
+ * begins and ends, and how tall it is. The skyline itself can be described by a list of (x, height)
+ * tuples, giving the locations at which the height visible to a distant observer changes, and each
+ * new height.
+ *
+ * Given an array of buildings as described above, create a function that returns the skyline.
+ *
+ * Leetcode: https://leetcode.com/problems/the-skyline-problem/
+ */
+// TODO:
