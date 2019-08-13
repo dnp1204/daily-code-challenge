4f76061731d91f79942532bf0704c301b52f997b
Doanh Pham <doanhpham1995@gmail.com>
2019-08-12 21:20:08 -0400

    add problem 294

---
+++ problems/200-299/problem-294.js
@@ -0,0 +1,11 @@
+/**
+ * Company: Square.
+ *
+ * A competitive runner would like to create a route that starts and ends at his house, with the condition
+ * that the route goes entirely uphill at first, and then entirely downhill.
+ *
+ * Given a dictionary of places of the form {location: elevation}, and a dictionary mapping paths between
+ * some of these locations to their corresponding distances, find the length of the shortest route satisfying
+ * the condition above. Assume the runner's home is location 0.
+ */
+// TODO:
