647b450db7288007fc66bec1205685786aef90e7
Doanh Pham <doanh.pham@vimeo.com>
2019-08-13 15:55:39 -0400

    add problem 295

--- problems/200-299/problem-295.js
+++ problems/200-299/problem-295.js
@@ -13,4 +13,15 @@
  *
  * Leetcode: https://leetcode.com/problems/pascals-triangle-ii/
  */
-// TODO:
+var getRow = function(rowIndex) {
+  let result = Array(rowIndex + 1).fill(0);
+  result[0] = 1;
+
+  for (let i = 1; i < rowIndex + 1; i++) {
+    for (let j = i; j >= 1; j--) {
+      result[j] += result[j - 1];
+    }
+  }
+
+  return result;
+};
