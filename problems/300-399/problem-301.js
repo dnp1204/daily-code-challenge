d1dd19919e08a5c197e64485d4c2526feb22780b
Doanh Pham <doanh.pham@vimeo.com>
2019-08-19 15:09:32 -0400

    solve problem 301

--- problems/300-399/problem-301.js
+++ problems/300-399/problem-301.js
@@ -8,5 +8,6 @@
  *
  * The check method may return occasional false positives (in other words, incorrectly identifying an element
  * as part of the set), but should always correctly identify a true element.
+ *
+ * Solution: https://github.com/vineetjohn/daily-coding-problem/blob/master/solutions/problem_301.py
  */
-// TODO:
