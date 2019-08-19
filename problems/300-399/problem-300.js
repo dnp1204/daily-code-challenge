5e28404410056073768f81504b1a9e2cb43abd5d
Doanh Pham <doanhpham1995@gmail.com>
2019-08-18 23:34:34 -0400

    solve problem 300

--- problems/300-399/problem-300.js
+++ problems/300-399/problem-300.js
@@ -5,4 +5,33 @@
  * Write a program that reads this file as a stream and returns the top 3 candidates at any given time.
  * If you find a voter voting more than once, report this as fraud.
  */
-// TODO:
+const fs = require('fs');
+const PriorityQueue = require('../../libraries/PriorityQueue');
+
+const getTop3Candidates = (fileName, nthTop) => {
+  const queue = new PriorityQueue([], (a, b) => a.count < b.count);
+  const counter = new Map();
+  const set = new Set();
+
+  const stream = fs.createReadStream(fileName);
+  fs.on('data', chunk => {
+    const { voterId, candidateId } = chunk;
+    if (set.has(voterId)) {
+      console.log(`${voterId} already voted`);
+    } else if (counter.has(candidateId)) {
+      counter.set(candidateId, counter.get(candidateId) + 1);
+    } else if (!counter.has(candidateId)) {
+      counter.set(candidateId, 1);
+    }
+
+    set.add(voterId);
+    queue.push({ candidateId, count: counter.get(candidateId) });
+    if (queue.length > nthTop) {
+      queue.pop();
+    }
+    console.log(`Top ${nthTop} current candidates`);
+    console.log(queue.data);
+  }).on('end', () => {
+    console.log(object);
+  });
+};
