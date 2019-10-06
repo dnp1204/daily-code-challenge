1d904cb3b0c16cc5b4822cb07774196360acb53a
Doanh Pham <doanhpham1995@gmail.com>
2019-10-06 16:00:25 -0400

    add problem 349

---
+++ problems/300-399/problem-349.js
@@ -0,0 +1,11 @@
+/**
+ * Company: Grammarly.
+ *
+ * Soundex is an algorithm used to categorize phonetically, such that two names that sound alike but are spelled differently have the same
+ * representation.
+ *
+ * Soundex maps every name to a string consisting of one letter and three numbers, like M460.
+ *
+ * Implement Soundex.
+ * TODO:
+ */
