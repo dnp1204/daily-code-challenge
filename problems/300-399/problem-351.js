363e3a3d1401a8dfa9ee28fb3445e7a7c49c5473
Doanh Pham <doanhpham1995@gmail.com>
2019-10-08 12:37:58 -0400

    add problem 351

---
+++ problems/300-399/problem-351.js
@@ -0,0 +1,11 @@
+/**
+ * Company: Quora.
+ *
+ * Word sense disambiguation is the problem of determining which sense a word takes on in a particular setting, if that word has multiple meanings.
+ * For example, in the sentence "I went to get money from the bank", bank probably means the place where people deposit money, not the land beside
+ * a river or lake.
+ *
+ * Given a sentence, most of whose words are contained in the meaning list above, create an algorithm that determines the likely sense of each possibly
+ * ambiguous word.
+ * TODO:
+ */
