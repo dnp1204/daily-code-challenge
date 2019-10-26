cf14681f3e8022b4ccbf7e441981de80dcbf7d2a
Doanh Pham <doanhpham1995@gmail.com>
2019-10-25 20:07:25 -0400

    add problem 2

--- README.md
+++ README.md
@@ -10,6 +10,18 @@ Bonus: Can you do this in one pass?
 
 ---
 
+## Problem 2 - Uber
+
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+
+[See Solution](/problems/1-99/problem-2.js)
+
+---
+
 ## Problem 33 - Square
 
 Given a list of words, return the shortest unique prefix of each word. For example, given the list:
--- problems/1-99/problem-15.js
+++ problems/1-99/problem-15.js
@@ -1,83 +1,36 @@
-/***
- * Company: Uber.
+/**
+ * Company: Amazon.
  *
- * Given an array of integers, return a new array such that each element at
- * index i of the new array is the product of all the numbers in the original
- * array except the one at i
+ * An sorted array of integers was rotated an unknown number of times.
+ * Given such an array, find the index of the element in the array in
+ * faster than linear time. If the element doesn't exist in the array,
+ * return null.
  *
- * For example, if the input was [1, 2, 3, 4, 5], the expected output would
- * be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expect output
- * would be [2, 3, 6]
+ * For example, given the array [13, 18, 25, 2, 8, 10] and the element 8,
+ * return 4 (the index of 8 in the array).
  *
- * Follow-up: what if you can't use division.
- *
- * Leetcode: https://leetcode.com/problems/product-of-array-except-self/
+ * You can assume all the integers in the array are unique.
  */
-function findProduct(array) {
-  if (array.length === 0) {
-    return [];
-  }
+var search = function(nums, target) {
+  let start = 0;
+  let end = nums.length;
 
-  let product = 1;
-  let hasZero = false;
+  while (start < end) {
+    const mid = Math.floor((start + end) / 2);
+    let num = target < nums[0] ? -Infinity : Infinity;
 
-  array.forEach(current => {
-    if (current !== 0) product *= current;
-    else hasZero = true;
-  });
-
-  const result = array.map(number => {
-    if (number === 0) {
-      return product;
-    } else if (hasZero) {
-      return 0;
+    if (nums[mid] < nums[0] == target < nums[0]) {
+      num = nums[mid];
     }
 
-    return product / number;
-  });
-
-  return result;
-}
-
-function findProductWithoutDivision(array) {
-  const left = [1];
-  const right = Array(array.length).fill(1);
-  const result = [];
-
-  for (let i = 1; i < array.length; i += 1) {
-    left[i] = left[i - 1] * array[i - 1];
+    if (num < target) {
+      start = mid + 1;
+    } else if (num > target) {
+      end = mid;
+    } else {
+      return mid;
+    }
   }
 
-  for (let i = array.length - 2; i >= 0; i -= 1) {
-    right[i] = right[i + 1] * array[i + 1];
-  }
-
-  for (let i = 0; i < array.length; i += 1) {
-    result.push(left[i] * right[i]);
-  }
-
-  return result;
-}
-
-const test1 = [1, 2, 3, 4, 5];
-const test2 = [];
-const test3 = [0, 2];
-const test4 = [2, 0];
-const test5 = [3, 2, 1];
-const test6 = [3, 2, 0, 1];
-
-console.log(findProduct(test1));
-console.log(findProduct(test2));
-console.log(findProduct(test3));
-console.log(findProduct(test4));
-console.log(findProduct(test5));
-console.log(findProduct(test6));
-
-console.log();
-
-console.log(findProductWithoutDivision(test1));
-console.log(findProductWithoutDivision(test2));
-console.log(findProductWithoutDivision(test3));
-console.log(findProductWithoutDivision(test4));
-console.log(findProductWithoutDivision(test5));
-console.log(findProductWithoutDivision(test6));
+  return -1;
+};
