a7056630114051d4e7985f1564d982a0fef02714
Doanh Pham <doanhpham1995@gmail.com>
2019-11-01 21:45:12 -0400

    add problem 9

--- README.md
+++ README.md
@@ -114,6 +114,18 @@ For example, the following tree has 5 unival subtrees:
 
 ---
 
+## Problem 9 - Airbnb [Difficulty: Hard]
+
+Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
+
+For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+
+Follow-up: Can you do this in O(N) time and constant space?
+
+[See Solution](/problems/1-99/problem-9.js)
+
+---
+
 ## Problem 33 - Square
 
 Given a list of words, return the shortest unique prefix of each word. For example, given the list:
--- problems/100-199/problem-165.js
+++ problems/100-199/problem-165.js
@@ -1,58 +1,53 @@
 /**
- * Company: Airbnb.
+ * Company: Google.
  *
- * Given a list of integers, write a function that returns the largest
- * sum of non-adjacent numbers. Numbers can be 0 or negative.
+ * Given an array of integers and a number k, where 1 <= k <= length of
+ * the array, compute the maximum values of each subarray of length k.
  *
- * For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6,
- * and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+ * For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should
+ * get: [10, 7, 8, 8], since:
+ * 10 = max(10, 5, 2)
+ * 7 = max(5, 2, 7)
+ * 8 = max(2, 7, 8)
+ * 8 = max(7, 8, 7)
  *
- * Follow-up: Can you do this in O(N) time and constant space?
+ * Do this in O(n) time and O(k) space. You can modify the input array
+ * in-place and you do not need to store the results. You can simply
+ * print them out as you compute them.
+ *
+ * Leetcode: https://leetcode.com/problems/sliding-window-maximum/
  */
-function findMaxNonAdjacent(nums) {
-  if (nums.length === 0) return -1;
-  if (nums.length === 1) return nums[0];
-  let max = Math.max(nums[0], nums[1]);
-  let mem = [nums[0], nums[1]];
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+  if (!nums.length || k <= 0) return [];
+  if (k === 1) return nums;
 
-  for (let i = 2; i < nums.length; i++) {
-    mem[i] = nums[i] + mem[i - 2];
-    if (i > 2) {
-      mem[i] = Math.max(mem[i], nums[i] + mem[i - 3]);
+  const queue = [];
+  const n = nums.length;
+  const r = Array(n - k + 1);
+  let ri = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    // remove numbers out of range k
+    while (queue.length && queue[0] < i - k + 1) {
+      queue.shift();
     }
-    max = Math.max(mem[i], max);
-  }
-  return max;
-}
 
-function findMaxNonAdjacent2(nums) {
-  if (nums.length === 0) return -1;
-  if (nums.length === 1) return nums[0];
-  let include = nums[0];
-  let exclude = 0;
+    // remove smaller numbers in k range as they are useless
+    while (queue.length && nums[queue[queue.length - 1]] < nums[i]) {
+      queue.pop();
+    }
 
-  for (let i = 1; i < nums.length; i++) {
-    let temp = Math.max(include, exclude);
-
-    include = exclude + nums[i];
-    exclude = temp;
+    // q contains index... r contains content
+    queue.push(i);
+    if (i >= k - 1) {
+      r[ri++] = nums[queue[0]];
+    }
   }
 
-  return Math.max(include, exclude);
-}
-
-console.log(findMaxNonAdjacent([5, 1, 1, 5]), 10);
-console.log(findMaxNonAdjacent([1, 5, 1, 5, 6]), 11);
-console.log(findMaxNonAdjacent([2, 4, 6, 2, 5]), 13);
-console.log(findMaxNonAdjacent([1, 20, 3]), 20);
-console.log(findMaxNonAdjacent([5, 5, 10, 100, 10, 5]), 110);
-console.log(findMaxNonAdjacent([3, 2, 5, 10, 7]), 15);
-console.log(findMaxNonAdjacent([5, 5, 10, 40, 50, 35]), 80);
-console.log();
-console.log(findMaxNonAdjacent2([5, 1, 1, 5]), 10);
-console.log(findMaxNonAdjacent2([1, 5, 1, 5, 6]), 11);
-console.log(findMaxNonAdjacent2([2, 4, 6, 2, 5]), 13);
-console.log(findMaxNonAdjacent2([1, 20, 3]), 20);
-console.log(findMaxNonAdjacent2([5, 5, 10, 100, 10, 5]), 110);
-console.log(findMaxNonAdjacent2([3, 2, 5, 10, 7]), 15);
-console.log(findMaxNonAdjacent2([5, 5, 10, 40, 50, 35]), 80);
+  return r;
+};
