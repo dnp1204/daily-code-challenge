6947cbe6e36845060da3821d44e5115bba6dcd60
Doanh Pham <doanhpham1995@gmail.com>
2020-01-13 10:35:55 -0500

    add problem 420

---
+++ problems/400-499/problem-420.js
@@ -0,0 +1,10 @@
+/**
+ * Company: Microsoft.
+ *
+ * A number is considered perfect if its digits sum up to exactly 10.
+ *
+ * Given a positive integer n, return the n-th perfect number.
+ *
+ * For example, given 1, you should return 19. Given 2, you should return 28.
+ * TODO:
+ */
