c2c8315c21f4aceaf9a5576e7385e3f24013a00c
Doanh Pham <doanhpham1995@gmail.com>
2020-05-02 20:10:59 -0400

    solve problem 62

--- problems/1-99/problem-62.js
+++ problems/1-99/problem-62.js
@@ -10,5 +10,50 @@
  * Down, then right
  *
  * Given a 5 by 5 matrix, there are 70 ways to get to the bottom-right.
- * TODO:
+ *
+ * Leetcode: https://leetcode.com/problems/unique-paths/
  */
+var uniquePaths = function (m, n) {
+  let total = 0;
+
+  const dp = function () {
+    if (m === 0 || n === 0) return 0;
+    const table = [];
+
+    for (let i = 0; i < m; i++) {
+      table[i] = [];
+
+      for (let j = 0; j < n; j++) {
+        if (i === 0) {
+          table[i][j] = 1;
+        } else {
+          table[i][j] = 0;
+        }
+      }
+      table[i][0] = 1;
+    }
+
+    for (let i = 1; i < m; i++) {
+      for (let j = 1; j < n; j++) {
+        table[i][j] = table[i - 1][j] + table[i][j - 1];
+      }
+    }
+
+    return table[m - 1][n - 1];
+  };
+
+  const helper = function (i, j) {
+    if (i > m - 1 || j > n - 1) return total;
+    if (i === m - 1 && j === n - 1) {
+      total += 1;
+      return total;
+    }
+
+    helper(i, j + 1);
+    helper(i + 1, j);
+
+    return total;
+  };
+
+  return dp();
+};
