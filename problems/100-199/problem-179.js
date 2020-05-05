5b76e702032474cbfb0fea57980b8d8dbf72d285
Doanh Pham <doanhpham1995@gmail.com>
2020-05-04 22:27:58 -0400

    implement 179

--- problems/100-199/problem-179.js
+++ problems/100-199/problem-179.js
@@ -1,6 +1,40 @@
 /**
  * Company: Google.
  *
- * Given the sequence of keys visited by a postorder traversal of a binary search tree, reconstruct the tree.
- * TODO:
+ * Given the sequence of keys visited by a post-order traversal of a binary search tree,
+ * reconstruct the tree.
+ *
+ * For example, given the sequence 2, 4, 3, 8, 7, 5, you should construct the following tree:
+ *
+ *     5
+ *    / \
+ *   3   7
+ *  / \   \
+ * 2   4   8
  */
+const { TreeNode } = require('../libraries/Tree');
+
+const constructTree = function(nums) {
+  let index = nums.length - 1;
+
+  const helper = function(key, min, max) {
+    if (index < 0) return null;
+    let root = null;
+
+    if (key > min && key < max) {
+      root = new TreeNode(key);
+
+      if (index - 1 >= 0) {
+        index -= 1;
+        root.right = helper(nums[index], key, max);
+        root.left = helper(nums[index], min, key);
+      }
+    }
+
+    return root;
+  };
+
+  return helper(nums[index], -Infinity, Infinity);
+};
+
+console.log(constructTree([2, 4, 3, 8, 7, 5]));
