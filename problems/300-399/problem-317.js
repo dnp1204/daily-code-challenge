6b39595507cc081ab482cb0800c9c05bf503c6a5
Isa Kurbanov <isakurbanov75@gmail.com>
2021-01-13 23:11:14 +0400

    Submitted problem-317

--- problems/300-399/problem-317.js
+++ problems/300-399/problem-317.js
@@ -2,5 +2,25 @@
  * Company: Yahoo.
  *
  * Write a function that returns the bitwise AND of all integers between M and N, inclusive.
- * TODO:
  */
+
+const mainFunc = (x, y) => {
+  let res = y;
+  
+  for (let i = x; i < y; ++i) {
+    res &= i;
+  }
+  return res;
+}
+
+
+// test
+let resultOne = mainFunc(193, 215);
+console.log(resultOne);
+
+let resultTwo = mainFunc(4, 5);
+console.log(resultTwo);
+
+let resultThree = mainFunc(100, 200);
+console.log(resultThree);
+
