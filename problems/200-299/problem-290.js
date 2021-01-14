51b5f93a0206165a4649fbc245476087c709d6b7
Isa Kurbanov <isakurbanov75@gmail.com>
2021-01-15 03:21:11 +0400

    Update problem-290.js

--- problems/200-299/problem-290.js
+++ problems/200-299/problem-290.js
@@ -8,4 +8,34 @@
  * Given N Quxes standing in a line, determine the smallest number of them remaining after any possible
  * sequence of such transformations.
  */
-// TODO:
+
+const subt = (color, vari) => {
+  let difference = color.filter(x => !vari.includes(x));
+
+  return difference[0];
+}
+
+const mainFunc = (data, length) => {
+  let color = ["R", "G", "B"];
+  let variation = [];
+
+  if (data.length === 0) {
+    return false;
+  }
+
+  for (var i = 0; i < data.length; ++i) {
+    for (var j = i + 1; j < data.length; ++j) {
+      variation.push(data[i], data[j])
+      let ret = subt(color, variation);
+      data.splice(0, 1);
+      data.splice(0, 1);
+      data.splice(0, 0, ret);
+      mainFunc(data, length - 1);
+    }
+  }
+  return data;
+}
+
+// test
+let val = ["R", "G", "B", "G", "B"];
+console.log(mainFunc(val, val.length));
