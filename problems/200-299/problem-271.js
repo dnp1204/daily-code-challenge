0de07d7cd252736d88dded82f5c88f4cfaa72a7f
Isa Kurbanov <isakurbanov75@gmail.com>
2021-01-18 15:35:30 +0400

    Update problem-271.js
    Quite hard problem. The tweak was to know binary search and to find middle element of an array, without bit-shift, division, or multiplication. This method seems good for this problem.

--- problems/200-299/problem-271.js
+++ problems/200-299/problem-271.js
@@ -6,4 +6,49 @@
  *
  * Do this in O(log N) time.
  */
-// TODO:
+
+const midEl = (arr, l, r) => {
+  let p = r + 1;
+  let q = l;
+  for (let i = 0; i < arr.length; ++i) {
+    p -= 1;
+    q += 1;
+    if (p == q) {
+      return p;
+    }
+    if (p < q) {
+      return p;
+    }
+  }
+  return q;
+};
+
+const binarySearch = (arr, l, r, x) => {
+  if (r >= l) {
+    let mid = +midEl(arr, l, r);
+
+    if (arr[mid] == x) {
+      return mid;
+    } else if (arr[mid] > x) {
+      return binarySearch(arr, l, mid - 1, x);
+    } else {
+      mid += 1;
+      return binarySearch(arr, mid++, r, x);
+    }
+  } else {
+    return -1;
+  }
+};
+
+// tests
+let array = [3, 7, 9, 15, 17, 20, 90];
+let lef = 0;
+let rig = array.length - 1;
+let res = binarySearch(array, lef, rig, 90);
+console.log(res);
+
+let arrayTwo = [5, 9, 21, 77, 120];
+let lefTwo = 0;
+let rigTwo = arrayTwo.length - 1;
+let resTwo = binarySearch(arrayTwo, lefTwo, rigTwo, 21);
+console.log(resTwo);
