7189f24d0a5e699b1a53f4ddb5d83e14d94d7f27
Doanh Pham <doanhpham1995@gmail.com>
2021-01-29 14:32:32 -0800

    added problem 463

---
+++ problems/400-499/problem-463.js
@@ -0,0 +1,11 @@
+/**
+ * Company: Facebook.
+ *
+ * On a mysterious island there are creatures known as Quxes which come in three colors: red, green, and blue.
+ * One power of the Qux is that if two of them are standing next to each other, they can transform into a single
+ * creature of the third color.
+ *
+ * Given N Quxes standing in a line, determine the smallest number of them remaining after any possible sequence
+ * of such transformations.
+ * TODO:
+ */
