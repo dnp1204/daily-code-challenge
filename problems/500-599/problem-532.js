0398f09a09a574f5f6adae9f95f64c5055291ddc
Doanh Pham <doanhpham1995@gmail.com>
2021-04-08 10:59:54 -0700

    added problem 532

---
+++ problems/500-599/problem-532.js
@@ -0,0 +1,11 @@
+/**
+ * Company: Google.
+ *
+ * On our special chessboard, two bishops attack each other if they share the same diagonal. This includes bishops
+ * that have another bishop located between them, i.e. bishops can attack through pieces.
+ *
+ * You are given N bishops, represented as (row, column) tuples on a M by M chessboard. Write a function to count
+ * the number of pairs of bishops that attack each other. The ordering of the pair doesn't matter: (1, 2) is considered
+ * the same as (2, 1).
+ * TODO:
+ */
